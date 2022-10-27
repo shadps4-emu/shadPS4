2c8cab1f0579c7e1a4f7df2648c65d373a07d87f
georgemoralis <giorgosmrls@gmail.com>
2022-10-27 13:03:57 +0300

    simple file handler class

---
+++ shadPS4/Types.h
@@ -0,0 +1,14 @@
+#pragma once
+
+using S08 = char;
+using S16 = short;
+using S32 = int;
+using S64 = long long;
+
+using U08 = unsigned char;
+using U16 = unsigned short;
+using U32 = unsigned int;
+using U64 = unsigned long long;
+
+using F32 = float;
+using F64 = double;
---
+++ shadPS4/core/FsFile.h
@@ -0,0 +1,64 @@
+#pragma once
+#include <cstdio>
+#include <string>
+#include "../Types.h"
+
+enum fsOpenMode
+{
+	fsRead = 0x1,
+	fsWrite = 0x2,
+	fsReadWrite = fsRead | fsWrite
+};
+
+enum fsSeekMode
+{
+	fsSeekSet,
+	fsSeekCur,
+	fsSeekEnd,
+};
+
+class FsFile
+{
+	std::FILE* m_file;
+public:
+	FsFile();
+	FsFile(const std::string& path, fsOpenMode mode = fsRead);
+	bool Open(const std::string& path, fsOpenMode mode = fsRead);
+	bool IsOpen() const;
+	bool Close();
+	bool Read(void* dst, U64 size);
+	U32 ReadBytes(void* dst, U64 size);
+	bool Write(const void* src, U64 size);
+	bool Seek(S64 offset, fsSeekMode mode);
+	U64 getFileSize();
+	U64 Tell() const;
+	~FsFile();
+
+
+	const char* getOpenMode(fsOpenMode mode)
+	{
+		switch (mode) {
+		case fsRead:        return "rb";
+		case fsWrite:       return "wb";
+		case fsReadWrite:   return "r+b";
+		}
+
+		return "r";
+	}
+
+	const int getSeekMode(fsSeekMode mode)
+	{
+		switch (mode) {
+		case fsSeekSet:  return SEEK_SET;
+		case fsSeekCur:  return SEEK_CUR;
+		case fsSeekEnd:  return SEEK_END;
+		}
+
+		return SEEK_SET;
+	}
+	std::FILE* fileDescr()
+	{
+		return m_file;
+	}
+};
+
