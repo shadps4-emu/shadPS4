380fb710b7ee961a13d55e5a14b42d1e92d1442f
georgemoralis <giorgosmrls@gmail.com>
2023-03-07 19:04:34 +0200

    some work on the new game list viewer

---
+++ shadPS4/emulator/gameInfo.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+struct GameInfo
+{
+	std::string path; //root path of game directory (normaly directory that contains eboot.bin)
+	std::string icon_path;//path of icon0.png
+
+	//variables extracted from param.sfo
+	std::string name = "Unknown";
+	std::string serial = "Unknown";
+	std::string app_ver = "Unknown";
+	std::string version = "Unknown";
+	std::string category = "Unknown";
+	std::string fw = "Unknown";
+
+};
---
+++ shadPS4/gui/custom_table_widget_item.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <QTableWidgetItem>
+
+class custom_table_widget_item : public QTableWidgetItem
+{
+private:
+	int m_sort_role = Qt::DisplayRole;
+
+public:
+	using QTableWidgetItem::setData;
+
+	custom_table_widget_item() = default;
+	custom_table_widget_item(const std::string& text, int sort_role = Qt::DisplayRole, const QVariant& sort_value = 0);
+	custom_table_widget_item(const QString& text, int sort_role = Qt::DisplayRole, const QVariant& sort_value = 0);
+
+	bool operator<(const QTableWidgetItem& other) const override;
+
+	void setData(int role, const QVariant& value, bool assign_sort_role);
+};
