8f36540386f18a0177af70e4d5427adc2c467356
georgemoralis <giorgosmrls@gmail.com>
2023-07-08 21:06:10 +0300

    kalitero usage minima sto main.cpp . Ola einai modules alla den tha katalaboun

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@
 int main(int argc, char* argv[])
 {
     if (argc == 1) {
-        printf("Usage: %s <module path>\n", argv[0]);
+        printf("Usage: %s <elf or eboot.bin path>\n", argv[0]);
         return -1;
     }
 
