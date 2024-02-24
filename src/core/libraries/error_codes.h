c7c3986c5797f4dfebfe052021d4745cadfd805c
georgemoralis <giorgosmrls@gmail.com>
2024-02-24 22:31:35 +0200

    some sample module code from autogen tool

---
+++ src/core/libraries/error_codes.h
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+// TODO placeholder until port finishes
+#include "core/hle/error_codes.h"
+
+constexpr int ORBIS_OK = 0;
