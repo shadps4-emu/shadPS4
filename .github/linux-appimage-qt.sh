2e6e94d75641955ac7a9e676039718c4f4f91039
georgemoralis <giorgosmrls@gmail.com>
2024-02-29 23:39:29 +0200

    ci fix?

--- .github/linux-appimage-qt.sh
+++ .github/linux-appimage-qt.sh
@@ -21,4 +21,4 @@ chmod a+x linuxdeploy-plugin-checkrt-x86_64.sh
 # Build AppImage
 ./linuxdeploy-x86_64.AppImage --appdir AppDir
 ./linuxdeploy-plugin-checkrt-x86_64.sh --appdir AppDir
-./linuxdeploy-x86_64.AppImage --appdir AppDir -d "$GITHUB_WORKSPACE"/.github/shadps4.desktop  -e "$GITHUB_WORKSPACE"/build/src/shadps4 -i "$GITHUB_WORKSPACE"/.github/shadps4.png --plugin qt --output appimage
+./linuxdeploy-x86_64.AppImage --appdir AppDir -d "$GITHUB_WORKSPACE"/.github/shadps4.desktop  -e "$GITHUB_WORKSPACE"/build/shadps4 -i "$GITHUB_WORKSPACE"/.github/shadps4.png --plugin qt --output appimage
