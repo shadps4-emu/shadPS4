0d1a1fae072663878db892481c96f2fb922539fa
Jonah <ckraziel@gmail.com>
2024-03-28 23:51:38 -0600

    Reuse..

--- src/qt_gui/game_list_frame.cpp
+++ src/qt_gui/game_list_frame.cpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include "game_list_frame.h"
 
 GameListFrame::GameListFrame(std::shared_ptr<GameInfoClass> game_info_get,
