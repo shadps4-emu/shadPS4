2b9b3e76bf6802d68ebc3d8ef21024468916e751
georgemoralis <giorgosmrls@gmail.com>
2024-05-04 19:08:46 +0300

    moved savedata to proper folder

--- src/core/libraries/save_data/savedata.cpp
+++ src/core/libraries/save_data/savedata.cpp
@@ -5,7 +5,7 @@
 #include "common/logging/log.h"
 #include "core/libraries/error_codes.h"
 #include "core/libraries/libs.h"
-#include "core/libraries/system/savedata.h"
+#include "core/libraries/save_data/savedata.h"
 
 namespace Libraries::SaveData {
 
