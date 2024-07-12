b07bbecab4f9cacc8566c04a376296c0018aa93d
georgemoralis <giorgosmrls@gmail.com>
2024-07-12 15:19:41 +0300

    replace unreachable with default return in sceKernelClockGettime

--- src/core/libraries/kernel/time_management.cpp
+++ src/core/libraries/kernel/time_management.cpp
@@ -97,7 +97,8 @@ int PS4_SYSV_ABI sceKernelClockGettime(s32 clock_id, OrbisKernelTimespec* tp) {
         pclock_id = CLOCK_MONOTONIC;
         break;
     default:
-        UNREACHABLE();
+        LOG_ERROR(Lib_Kernel, "unsupported = {} using CLOCK_REALTIME", clock_id);
+        break;
     }
 
     timespec t{};
