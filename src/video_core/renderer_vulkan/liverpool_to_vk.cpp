fcbaec51df8d75b7ba23f30534235768956777ce
IndecisiveTurtle <47210458+raphaelthegreat@users.noreply.github.com>
2024-07-17 16:19:08 +0300

    liverpool_to_vk: Add more surface formats

--- src/video_core/renderer_vulkan/liverpool_to_vk.cpp
+++ src/video_core/renderer_vulkan/liverpool_to_vk.cpp
@@ -441,6 +441,18 @@ vk::Format SurfaceFormat(AmdGpu::DataFormat data_format, AmdGpu::NumberFormat nu
         num_format == AmdGpu::NumberFormat::Unorm) {
         return vk::Format::eR16G16B16A16Unorm;
     }
+    if (data_format == AmdGpu::DataFormat::Format8 &&
+        num_format == AmdGpu::NumberFormat::Uint) {
+        return vk::Format::eR8Uint;
+    }
+    if (data_format == AmdGpu::DataFormat::Format16_16_16_16 &&
+        num_format == AmdGpu::NumberFormat::SnormNz) {
+        return vk::Format::eR16G16B16A16Snorm;
+    }
+    if (data_format == AmdGpu::DataFormat::FormatBc6 &&
+        num_format == AmdGpu::NumberFormat::Unorm) {
+        return vk::Format::eBc6HUfloatBlock;
+    }
     UNREACHABLE_MSG("Unknown data_format={} and num_format={}", u32(data_format), u32(num_format));
 }
 
