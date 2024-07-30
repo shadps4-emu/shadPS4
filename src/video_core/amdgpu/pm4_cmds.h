ac557c1e5ae44a342a9c4b623e3f6b3259d5ff3c
DanielSvoboda <daniel.svoboda@hotmail.com>
2024-07-30 12:24:52 -0300

    Update pm4_cmds.h

--- src/video_core/amdgpu/pm4_cmds.h
+++ src/video_core/amdgpu/pm4_cmds.h
@@ -312,13 +312,6 @@ struct PM4CmdEventWriteEop {
         return data_lo | u64(data_hi) << 32;
     }
 
-    uint64_t GetGpuClock64() const {
-        auto now = std::chrono::high_resolution_clock::now();
-        auto duration = now.time_since_epoch();
-        auto ticks = std::chrono::duration_cast<std::chrono::nanoseconds>(duration).count();
-        return static_cast<uint64_t>(ticks);
-    }
-
     void SignalFence() const {
         switch (data_sel.Value()) {
         case DataSelect::None: {
@@ -333,7 +326,7 @@ struct PM4CmdEventWriteEop {
             break;
         }
         case DataSelect::GpuClock64: {
-            *Address<u64>() = GetGpuClock64();
+            // Todo
             break;
         }
         case DataSelect::PerfCounter: {
