2eee78a4645941e1042cf8f6d48e1a2325157ad0
microsoftv <6063922+microsoftv@users.noreply.github.com>
2024-08-17 14:37:48 -0400

    distinguish RTN opcodes

--- src/shader_recompiler/frontend/translate/translate.h
+++ src/shader_recompiler/frontend/translate/translate.h
@@ -197,9 +197,9 @@ public:
     void DS_SWIZZLE_B32(const GcnInst& inst);
     void DS_READ(int bit_size, bool is_signed, bool is_pair, const GcnInst& inst);
     void DS_WRITE(int bit_size, bool is_signed, bool is_pair, const GcnInst& inst);
-    void DS_MAX_U32(const GcnInst& inst);
-    void DS_MIN_U32(const GcnInst& inst);
-    void DS_ADD_U32(const GcnInst& inst);
+    void DS_ADD_U32(const GcnInst& inst, bool rtn);
+    void DS_MIN_U32(const GcnInst& inst, bool rtn);
+    void DS_MAX_U32(const GcnInst& inst, bool rtn);
     void V_READFIRSTLANE_B32(const GcnInst& inst);
     void V_READLANE_B32(const GcnInst& inst);
     void V_WRITELANE_B32(const GcnInst& inst);
