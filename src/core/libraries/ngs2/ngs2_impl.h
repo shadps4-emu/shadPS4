63dcb621412ea08368f3a225f36a6e28fc750bb0
microsoftv <6063922+microsoftv@users.noreply.github.com>
2024-08-18 19:10:44 -0400

    mac

--- src/core/libraries/ngs2/ngs2_impl.h
+++ src/core/libraries/ngs2/ngs2_impl.h
@@ -11,13 +11,13 @@ class Ngs2 {
 public:
     s32 ReportInvalid(u32 handle_type) const;
     s32 HandleSetup(Ngs2Handle* handle, void* data, std::atomic<u32>* atomic, u32 type, u32 flags);
-    s32 Ngs2::HandleCleanup(Ngs2Handle* handle, u32 hType, void* dataOut);
-    s32 Ngs2::HandleEnter(Ngs2Handle* handle, u32 hType, Ngs2Handle* handleOut);
-    s32 Ngs2::HandleLeave(Ngs2Handle* handle);
-    s32 Ngs2::StackBufferOpen(StackBuffer* buf, void* base_addr, size_t size, void** stackTop,
+    s32 HandleCleanup(Ngs2Handle* handle, u32 hType, void* dataOut);
+    s32 HandleEnter(Ngs2Handle* handle, u32 hType, Ngs2Handle* handleOut);
+    s32 HandleLeave(Ngs2Handle* handle);
+    s32 StackBufferOpen(StackBuffer* buf, void* base_addr, size_t size, void** stackTop,
                               bool verify);
-    s32 Ngs2::StackBufferClose(StackBuffer* buf, size_t* usedSize);
-    s32 Ngs2::SystemSetupCore(StackBuffer* buf, SystemOptions* options, Ngs2Handle** sysOut);
+    s32 StackBufferClose(StackBuffer* buf, size_t* usedSize);
+    s32 SystemSetupCore(StackBuffer* buf, SystemOptions* options, Ngs2Handle** sysOut);
 
 private:
 };
