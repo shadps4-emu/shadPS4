6a2153da99266ca5329e322a246c5ca17472cc82
Lizardy <reptilian532@hotmail.com>
2024-08-20 14:56:08 -0400

    log handle addr

--- src/core/libraries/ngs2/ngs2_impl.cpp
+++ src/core/libraries/ngs2/ngs2_impl.cpp
@@ -13,21 +13,22 @@ using namespace Libraries::Kernel;
 namespace Libraries::Ngs2 {
 
 s32 Ngs2::ReportInvalid(Ngs2Handle* handle, u32 handle_type) const {
+    uintptr_t hAddress = reinterpret_cast<uintptr_t>(handle);
     switch (handle_type) {
     case 1:
-        LOG_ERROR(Lib_Ngs2, "Invalid system handle {}", &handle);
+        LOG_ERROR(Lib_Ngs2, "Invalid system handle {}", hAddress);
         return ORBIS_NGS2_ERROR_INVALID_SYSTEM_HANDLE;
     case 2:
-        LOG_ERROR(Lib_Ngs2, "Invalid rack handle {}", handle_type);
+        LOG_ERROR(Lib_Ngs2, "Invalid rack handle {}", hAddress);
         return ORBIS_NGS2_ERROR_INVALID_RACK_HANDLE;
     case 4:
-        LOG_ERROR(Lib_Ngs2, "Invalid voice handle {}", handle_type);
+        LOG_ERROR(Lib_Ngs2, "Invalid voice handle {}", hAddress);
         return ORBIS_NGS2_ERROR_INVALID_VOICE_HANDLE;
     case 8:
-        LOG_ERROR(Lib_Ngs2, "Invalid report handle {}", handle_type);
+        LOG_ERROR(Lib_Ngs2, "Invalid report handle {}", hAddress);
         return ORBIS_NGS2_ERROR_INVALID_REPORT_HANDLE;
     default:
-        LOG_ERROR(Lib_Ngs2, "Invalid handle {}", handle_type);
+        LOG_ERROR(Lib_Ngs2, "Invalid handle {}", );
         return ORBIS_NGS2_ERROR_INVALID_HANDLE;
     }
 }
