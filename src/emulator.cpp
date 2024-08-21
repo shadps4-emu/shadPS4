0ddf11e062b14e5751f6f479625a3b08e9b0c180
Aiden Turner <yosefaiden@gmail.com>
2024-08-21 17:03:17 -0400

    fixed compile error

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -63,7 +63,7 @@ Emulator::Emulator() {
     LOG_INFO(Config, "Vulkan vkValidationGpu: {}", Config::vkValidationGpuEnabled());
     LOG_INFO(Config, "Vulkan rdocEnable: {}", Config::isRdocEnabled());
     LOG_INFO(Config, "Vulkan rdocMarkersEnable: {}", Config::isMarkersEnabled());
-    LOG_INFO(Config, "LLE isLibc: {}," Config::isLleLibc());
+    LOG_INFO(Config, "LLE isLibc: {}", Config::isLleLibc());
 
     // Defer until after logging is initialized.
     memory = Core::Memory::Instance();
