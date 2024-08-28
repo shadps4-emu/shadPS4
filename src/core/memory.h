905d49fd9691cab31e0a66eb94e3e0181679dbb1
squidbus <175574877+squidbus@users.noreply.github.com>
2024-08-28 03:42:53 -0700

    memory: Fixes for direct memory allocation. (#615)
    * memory: Size direct memory based on requested flexible memory.
    
    * memory: Guard against OrbisProcParam without an OrbisKernelMemParam.
    
    * memory: Account for alignment in direct memory suitability checks and add more debugging.

--- src/core/memory.h
+++ src/core/memory.h
@@ -130,8 +130,8 @@ public:
         rasterizer = rasterizer_;
     }
 
-    void SetTotalFlexibleSize(u64 size) {
-        total_flexible_size = size;
+    u64 GetTotalDirectSize() const {
+        return total_direct_size;
     }
 
     u64 GetAvailableFlexibleSize() const {
@@ -141,6 +141,8 @@ public:
     VAddr SystemReservedVirtualBase() noexcept {
         return impl.SystemReservedVirtualBase();
     }
+
+    void SetupMemoryRegions(u64 flexible_size);
 
     PAddr Allocate(PAddr search_start, PAddr search_end, size_t size, u64 alignment,
                    int memory_type);
@@ -217,7 +219,8 @@ private:
     DMemMap dmem_map;
     VMAMap vma_map;
     std::recursive_mutex mutex;
-    size_t total_flexible_size = 448_MB;
+    size_t total_direct_size{};
+    size_t total_flexible_size{};
     size_t flexible_usage{};
     Vulkan::Rasterizer* rasterizer{};
 };
