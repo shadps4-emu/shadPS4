e2d8a48855ce9cc65a68eadc2457151c993309c8
Antonio <mena120600@gmail.com>
2024-09-04 16:16:34 -0400

    ''

--- src/core/memory.cpp
+++ src/core/memory.cpp
@@ -365,7 +365,7 @@ int MemoryManager::MTypeProtect(VAddr addr, size_t size, VMAType mtype, MemoryPr
         return ORBIS_KERNEL_ERROR_EINVAL;
     }
 
-    // Validate protection flag
+    // Validate protection flags
     constexpr static MemoryProt valid_flags = MemoryProt::NoAccess | MemoryProt::CpuRead |
                                               MemoryProt::CpuReadWrite | MemoryProt::GpuRead |
                                               MemoryProt::GpuWrite | MemoryProt::GpuReadWrite;
