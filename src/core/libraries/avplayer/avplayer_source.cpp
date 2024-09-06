9291b673a00c93f722cba809d6636f9ac7d4a49a
zmckevitt <zack.mckevitt@gmail.com>
2024-09-06 12:52:30 -0600

    fixed formatting

--- src/core/libraries/avplayer/avplayer_source.cpp
+++ src/core/libraries/avplayer/avplayer_source.cpp
@@ -20,7 +20,7 @@ extern "C" {
 #include <libswscale/swscale.h>
 }
 
-// The av_err2str macro in libavutil/error.h does not play nice with C++ 
+// The av_err2str macro in libavutil/error.h does not play nice with C++
 #ifdef av_err2str
 #undef av_err2str
 #include <string>
@@ -29,7 +29,7 @@ av_always_inline std::string av_err2string(int errnum) {
     return av_make_error_string(errbuf, AV_ERROR_MAX_STRING_SIZE, errnum);
 }
 #define av_err2str(err) av_err2string(err).c_str()
-#endif  // av_err2str
+#endif // av_err2str
 
 namespace Libraries::AvPlayer {
 
