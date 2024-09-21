4126eb647d73ad6dbe15862ad721c33cdb685e83
squidbus <175574877+squidbus@users.noreply.github.com>
2024-09-21 15:21:29 -0700

    shader_recompiler: Use SetDst in more instructions.

--- src/shader_recompiler/frontend/translate/vector_interpolation.cpp
+++ src/shader_recompiler/frontend/translate/vector_interpolation.cpp
@@ -22,17 +22,15 @@ void Translator::EmitVectorInterpolation(const GcnInst& inst) {
 // VINTRP
 
 void Translator::V_INTERP_P2_F32(const GcnInst& inst) {
-    const IR::VectorReg dst_reg{inst.dst[0].code};
     auto& attr = runtime_info.fs_info.inputs.at(inst.control.vintrp.attr);
     const IR::Attribute attrib{IR::Attribute::Param0 + attr.param_index};
-    ir.SetVectorReg(dst_reg, ir.GetAttribute(attrib, inst.control.vintrp.chan));
+    SetDst(inst.dst[0], ir.GetAttribute(attrib, inst.control.vintrp.chan));
 }
 
 void Translator::V_INTERP_MOV_F32(const GcnInst& inst) {
-    const IR::VectorReg dst_reg{inst.dst[0].code};
     auto& attr = runtime_info.fs_info.inputs.at(inst.control.vintrp.attr);
     const IR::Attribute attrib{IR::Attribute::Param0 + attr.param_index};
-    ir.SetVectorReg(dst_reg, ir.GetAttribute(attrib, inst.control.vintrp.chan));
+    SetDst(inst.dst[0], ir.GetAttribute(attrib, inst.control.vintrp.chan));
 }
 
 } // namespace Shader::Gcn
