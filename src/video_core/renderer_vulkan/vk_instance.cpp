1f59e3663cd34f44c4fe72b44473ade12b7b8729
squidbus <175574877+squidbus@users.noreply.github.com>
2024-09-20 22:10:55 -0700

    vulkan: Use closest available equivalent to missing clamp modes.

--- src/video_core/renderer_vulkan/vk_instance.cpp
+++ src/video_core/renderer_vulkan/vk_instance.cpp
@@ -309,6 +309,7 @@ bool Instance::CreateDevice() {
             .separateDepthStencilLayouts = vk12_features.separateDepthStencilLayouts,
             .hostQueryReset = vk12_features.hostQueryReset,
             .timelineSemaphore = vk12_features.timelineSemaphore,
+            .samplerMirrorClampToEdge = vk12_features.samplerMirrorClampToEdge,
         },
         vk::PhysicalDeviceMaintenance4FeaturesKHR{
             .maintenance4 = true,
