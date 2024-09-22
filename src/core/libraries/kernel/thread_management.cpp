d49b0ba4f2bc78482e8e609628d3364b5711fb80
tGecko <tGecko@proton.me>
2024-09-22 22:09:53 +0200

    Add nullptr check in scePthreadSetprio function

--- src/core/libraries/kernel/thread_management.cpp
+++ src/core/libraries/kernel/thread_management.cpp
@@ -1512,6 +1512,10 @@ int PS4_SYSV_ABI scePthreadGetprio(ScePthread thread, int* prio) {
     return ORBIS_OK;
 }
 int PS4_SYSV_ABI scePthreadSetprio(ScePthread thread, int prio) {
+    if (thread == nullptr) {
+        LOG_ERROR(Kernel_Pthread, "scePthreadSetprio: thread is nullptr");
+        return ORBIS_KERNEL_ERROR_EINVAL;
+    }
     thread->prio = prio;
     return ORBIS_OK;
 }
