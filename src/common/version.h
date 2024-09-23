a016792371ac052f20f677c56fc580b7fd065335
georgemoralis <giorgosmrls@gmail.com>
2024-09-23 18:42:11 +0300

    starting 0.3.1 WIP

--- src/common/version.h
+++ src/common/version.h
@@ -8,7 +8,7 @@
 
 namespace Common {
 
-constexpr char VERSION[] = "0.3.0";
-constexpr bool isRelease = true;
+constexpr char VERSION[] = "0.3.1 WIP";
+constexpr bool isRelease = false;
 
 } // namespace Common
