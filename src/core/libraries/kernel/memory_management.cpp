9088a91ceb0718bd3d7815134c1b4219577898f2
dbz400 <jacky.kk.tsui@gmail.com>
2024-09-29 01:16:46 +0800

    clang fix

--- src/core/libraries/kernel/memory_management.cpp
+++ src/core/libraries/kernel/memory_management.cpp
@@ -24,7 +24,8 @@ u64 PS4_SYSV_ABI sceKernelGetDirectMemorySize() {
 int PS4_SYSV_ABI sceKernelAllocateDirectMemory(s64 searchStart, s64 searchEnd, u64 len,
                                                u64 alignment, int memoryType, s64* physAddrOut) {
 
-    LOG_INFO(Kernel_Vmm,"searchStart = {:#x}, searchEnd = {:#x}, len = {:#x}, "
+    LOG_INFO(Kernel_Vmm,
+             "searchStart = {:#x}, searchEnd = {:#x}, len = {:#x}, "
              "alignment = {:#x}, memoryType = {:#x}, physAddrOut = {:#x}",
              searchStart, searchEnd, len, alignment, memoryType, phys_addr);
     
