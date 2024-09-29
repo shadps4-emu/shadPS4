c49bbebff8b16a71ac76de4cd022ef1e04936eb1
squidbus <175574877+squidbus@users.noreply.github.com>
2024-09-28 19:45:00 -0700

    vulkan: Fix primitive list restart validation error.

--- src/video_core/renderer_vulkan/vk_instance.h
+++ src/video_core/renderer_vulkan/vk_instance.h
@@ -143,6 +143,10 @@ public:
         return maintenance5;
     }
 
+    bool IsListRestartSupported() const {
+        return list_restart;
+    }
+
     /// Returns the vendor ID of the physical device
     u32 GetVendorID() const {
         return properties.vendorID;
@@ -286,6 +290,7 @@ private:
     bool vertex_input_dynamic_state{};
     bool null_descriptor{};
     bool maintenance5{};
+    bool list_restart{};
     u64 min_imported_host_pointer_alignment{};
     u32 subgroup_size{};
     bool tooling_info{};
