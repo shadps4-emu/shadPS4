55f4e39506361c816c391828b6dbd46cfbc1df89
delledev <lucadelle2@gmail.com>
2024-10-01 17:42:22 +0300

    linting issues

--- src/qt_gui/discord_rpc_handler.cpp
+++ src/qt_gui/discord_rpc_handler.cpp
@@ -6,38 +6,38 @@
 #include "src/qt_gui/discord_rpc_handler.h"
 
 namespace DiscordRPCHandler {
-	
-	void RPC::init() {
-		DiscordEventHandlers handlers{};
-        std::string discordAppId = "1290207945476280360";
-        Discord_Initialize(discordAppId.c_str(), &handlers, 1, nullptr);
-        startTimestamp = time(nullptr);
-        enabled = true;
-	}
 
-	void RPC::setStatusIdling() {
-        DiscordRichPresence rpc{};
-        rpc.largeImageKey = "https://github.com/shadps4-emu/shadPS4/raw/main/.github/shadps4.png";
-        rpc.largeImageText = "ShadPS4 is a PS4 emulator";
-        rpc.startTimestamp = startTimestamp;
-        rpc.details = "Idle";
+void RPC::init() {
+    DiscordEventHandlers handlers{};
+    std::string discordAppId = "1290207945476280360";
+    Discord_Initialize(discordAppId.c_str(), &handlers, 1, nullptr);
+    startTimestamp = time(nullptr);
+    enabled = true;
+}
 
-        Discord_UpdatePresence(&rpc);
-    }
+void RPC::setStatusIdling() {
+    DiscordRichPresence rpc{};
+    rpc.largeImageKey = "https://github.com/shadps4-emu/shadPS4/raw/main/.github/shadps4.png";
+    rpc.largeImageText = "ShadPS4 is a PS4 emulator";
+    rpc.startTimestamp = startTimestamp;
+    rpc.details = "Idle";
 
-    void RPC::setStatusPlaying(const std::string& game_name, const std::string& game_id) {
-        DiscordRichPresence rpc{};
+    Discord_UpdatePresence(&rpc);
+}
 
-        rpc.details = "Playing";
-        rpc.state = game_name.c_str();
-        std::string largeImageUrl =
-            "https://store.playstation.com/store/api/chihiro/00_09_000/titlecontainer/US/en/999/" +
-            game_id + "_00/image";
-        rpc.largeImageKey = largeImageUrl.c_str();
-        rpc.largeImageText = game_name.c_str();
-        rpc.startTimestamp = startTimestamp;
+void RPC::setStatusPlaying(const std::string& game_name, const std::string& game_id) {
+    DiscordRichPresence rpc{};
 
-        Discord_UpdatePresence(&rpc);
-    }
+    rpc.details = "Playing";
+    rpc.state = game_name.c_str();
+    std::string largeImageUrl =
+        "https://store.playstation.com/store/api/chihiro/00_09_000/titlecontainer/US/en/999/" +
+        game_id + "_00/image";
+    rpc.largeImageKey = largeImageUrl.c_str();
+    rpc.largeImageText = game_name.c_str();
+    rpc.startTimestamp = startTimestamp;
+
+    Discord_UpdatePresence(&rpc);
+}
 
 } // namespace DiscordRPCHandler
