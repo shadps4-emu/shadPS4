5e26294e27715415884e4b64f98ffd9817621c0a
Daniel R. <47796739+polybiusproxy@users.noreply.github.com>
2024-10-03 16:57:33 +0200

    video_core: disable warnings on vulkan

--- src/video_core/renderer_vulkan/vk_common.h
+++ src/video_core/renderer_vulkan/vk_common.h
@@ -17,7 +17,11 @@
 #define VULKAN_HPP_NO_EXCEPTIONS
 // Define assert-on-result to nothing to instead return the result for our handling.
 #define VULKAN_HPP_ASSERT_ON_RESULT
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-value"
 #include <vulkan/vulkan.hpp>
+#pragma clang diagnostic pop
 
 #define VMA_STATIC_VULKAN_FUNCTIONS 0
 #define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
