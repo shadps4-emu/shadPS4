87ecb87674e493ecef9e18336ab01a7e13cde612
Vinicius Rangel <me@viniciusrangel.dev>
2024-10-12 21:46:10 -0300

    imgui: skip all textures to avoid hanging with crash diagnostic enabled
    not sure why this happens :c

--- src/imgui/renderer/texture_manager.cpp
+++ src/imgui/renderer/texture_manager.cpp
@@ -7,6 +7,7 @@
 #include <externals/stb_image.h>
 
 #include "common/assert.h"
+#include "common/config.h"
 #include "common/io_file.h"
 #include "common/polyfill_thread.h"
 #include "imgui_impl_vulkan.h"
@@ -146,6 +147,11 @@ void WorkerLoop() {
             auto [core, png_raw, path] = std::move(g_job_list.front());
             g_job_list.pop_front();
             g_job_list_mtx.unlock();
+
+            if (Config::vkCrashDiagnosticEnabled()) {
+                // FIXME: Crash diagnostic hangs when building the command buffer here
+                continue;
+            }
 
             if (!path.empty()) { // Decode PNG from file
                 Common::FS::IOFile file(path, Common::FS::FileAccessMode::Read);
