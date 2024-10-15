7b9769ee469f70eadc316d5f2e7619788b9c2680
ElBread3 <92335081+ElBread3@users.noreply.github.com>
2024-10-15 10:47:15 -0500

    use operator

--- src/core/file_sys/fs.cpp
+++ src/core/file_sys/fs.cpp
@@ -56,7 +56,8 @@ std::filesystem::path MntPoints::GetHostPath(std::string_view guest_directory, b
     const auto rel_path = std::string_view(corrected_path).substr(pos);
     std::filesystem::path host_path = mount->host_path / rel_path;
 
-    std::filesystem::path patch_path = mount->host_path.string() + "-UPDATE";
+    std::filesystem::path patch_path = mount->host_path;
+    patch_path += "-UPDATE";
     if (corrected_path.starts_with("/app0/") && std::filesystem::exists(patch_path / rel_path)) {
         host_path = patch_path / rel_path;
     }
