e952013fe06b6d040d3be091e3c7e50e3456770f
Mahmoud Adel <94652220+AboMedoz@users.noreply.github.com>
2024-12-29 12:47:15 +0200

    add EventWrite and DispatchIndirect to ProcessCompute (#1948)
    * add EventWrite and DispatchIndirect to ProcessCompute
    
    helps Alienation go Ingame
    
    * apply review changes
    
    Co-authored-by: TheTurtle <47210458+raphaelthegreat@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: TheTurtle <47210458+raphaelthegreat@users.noreply.github.com>

--- src/video_core/amdgpu/liverpool.cpp
+++ src/video_core/amdgpu/liverpool.cpp
@@ -821,6 +821,24 @@ Liverpool::Task Liverpool::ProcessCompute(std::span<const u32> acb, u32 vqid) {
             }
             break;
         }
+        case PM4ItOpcode::DispatchIndirect: {
+            const auto* dispatch_indirect = reinterpret_cast<const PM4CmdDispatchIndirect*>(header);
+            auto& cs_program = GetCsRegs();
+            const auto offset = dispatch_indirect->data_offset;
+            const auto ib_address = mapped_queues[GfxQueueId].indirect_args_addr;
+            const auto size = sizeof(PM4CmdDispatchIndirect::GroupDimensions);
+            if (DebugState.DumpingCurrentReg()) {
+                DebugState.PushRegsDumpCompute(base_addr, reinterpret_cast<uintptr_t>(header),
+                                               cs_program);
+            }
+            if (rasterizer && (cs_program.dispatch_initiator & 1)) {
+                const auto cmd_address = reinterpret_cast<const void*>(header);
+                rasterizer->ScopeMarkerBegin(fmt::format("acb:{}:DispatchIndirect", cmd_address));
+                rasterizer->DispatchIndirect(ib_address, offset, size);
+                rasterizer->ScopeMarkerEnd();
+            }
+            break;
+        }
         case PM4ItOpcode::WriteData: {
             const auto* write_data = reinterpret_cast<const PM4CmdWriteData*>(header);
             ASSERT(write_data->dst_sel.Value() == 2 || write_data->dst_sel.Value() == 5);
@@ -843,6 +861,10 @@ Liverpool::Task Liverpool::ProcessCompute(std::span<const u32> acb, u32 vqid) {
         case PM4ItOpcode::ReleaseMem: {
             const auto* release_mem = reinterpret_cast<const PM4CmdReleaseMem*>(header);
             release_mem->SignalFence(static_cast<Platform::InterruptId>(queue.pipe_id));
+            break;
+        }
+        case PM4ItOpcode::EventWrite: {
+            // const auto* event = reinterpret_cast<const PM4CmdEventWrite*>(header);
             break;
         }
         default:
