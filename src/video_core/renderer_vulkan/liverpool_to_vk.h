e7c5d3ba86d9f6034172fb876bbc00e59678fb82
Xphalnos <164882787+Xphalnos@users.noreply.github.com>
2024-12-31 18:54:18 +0100

    Fix PromoteFormatToDepth

--- src/video_core/renderer_vulkan/liverpool_to_vk.h
+++ src/video_core/renderer_vulkan/liverpool_to_vk.h
@@ -76,6 +76,8 @@ static inline vk::Format PromoteFormatToDepth(vk::Format fmt) {
         return vk::Format::eD32Sfloat;
     } else if (fmt == vk::Format::eR16Unorm) {
         return vk::Format::eD16Unorm;
+    } else if (fmt == vk::Format::eR8G8B8A8Unorm) {
+        return vk::Format::eR32Uint;
     }
     UNREACHABLE();
 }
