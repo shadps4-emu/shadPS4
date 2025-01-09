7701bf3f54fb4d300d12a0e008b070530c69d99b
Stephen <millerste004@gmail.com>
2025-01-09 11:59:16 -0600

    NpCommon stubs
    A couple of commonly spammed stubs come from here.

--- src/common/logging/types.h
+++ src/common/logging/types.h
@@ -65,6 +65,7 @@ enum class Class : u8 {
     Lib_Ssl,               ///< The LibSceSsl implementation.
     Lib_Http,              ///< The LibSceHttp implementation.
     Lib_SysModule,         ///< The LibSceSysModule implementation
+    Lib_NpCommon,          ///< The LibSceNpCommon implementation
     Lib_NpManager,         ///< The LibSceNpManager implementation
     Lib_NpScore,           ///< The LibSceNpScore implementation
     Lib_NpTrophy,          ///< The LibSceNpTrophy implementation
