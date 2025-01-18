b48c445bd42f4b7c5d3630c524e7726aeee2ea26
Quang Ngô <ngoquang2708@gmail.com>
2025-01-18 13:57:44 +0700

    Remove duplicated rendering_info declarations

--- src/video_core/renderer_vulkan/vk_presenter.cpp
+++ src/video_core/renderer_vulkan/vk_presenter.cpp
@@ -608,6 +608,16 @@ Frame* Presenter::PrepareFrameInternal(VideoCore::ImageId image_id, bool is_eop)
         .loadOp = vk::AttachmentLoadOp::eClear,
         .storeOp = vk::AttachmentStoreOp::eStore,
     }};
+    const vk::RenderingInfo rendering_info{
+        .renderArea =
+            vk::Rect2D{
+                .offset = {0, 0},
+                .extent = {frame->width, frame->height},
+            },
+        .layerCount = 1,
+        .colorAttachmentCount = attachments.size(),
+        .pColorAttachments = attachments.data(),
+    };
 
     if (image_id != VideoCore::NULL_IMAGE_ID) {
         auto& image = texture_cache.GetImage(image_id);
@@ -669,31 +679,11 @@ Frame* Presenter::PrepareFrameInternal(VideoCore::ImageId image_id, bool is_eop)
         cmdbuf.pushConstants(*pp_pipeline_layout, vk::ShaderStageFlagBits::eFragment, 0,
                              sizeof(PostProcessSettings), &pp_settings);
 
-        vk::RenderingInfo rendering_info{
-            .renderArea =
-                vk::Rect2D{
-                    .offset = {0, 0},
-                    .extent = {frame->width, frame->height},
-                },
-            .layerCount = 1,
-            .colorAttachmentCount = attachments.size(),
-            .pColorAttachments = attachments.data(),
-        };
         cmdbuf.beginRendering(rendering_info);
         cmdbuf.draw(3, 1, 0, 0);
         cmdbuf.endRendering();
     } else {
         // Fix display of garbage images on startup on some drivers
-        vk::RenderingInfo rendering_info{
-            .renderArea =
-                vk::Rect2D{
-                    .offset = {0, 0},
-                    .extent = {frame->width, frame->height},
-                },
-            .layerCount = 1,
-            .colorAttachmentCount = attachments.size(),
-            .pColorAttachments = attachments.data(),
-        };
         cmdbuf.beginRendering(rendering_info);
         cmdbuf.endRendering();
     }
