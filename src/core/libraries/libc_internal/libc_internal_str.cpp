00d916208900671e55a6b78a0a20e98cd3970cca
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2025-01-29 12:13:50 +0100

    Empty str and memory files

---
+++ src/core/libraries/libc_internal/libc_internal_str.cpp
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "common/assert.h"
+#include "common/logging/log.h"
+#include "core/libraries/error_codes.h"
+#include "core/libraries/libs.h"
+#include "libc_internal_str.h"
+
+namespace Libraries::LibcInternal {
+
+void RegisterlibSceLibcInternalStr(Core::Loader::SymbolsResolver* sym) {
+
+}
+
+}
