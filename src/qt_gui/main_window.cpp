adbbca3e9bc12bb12b7191acd50fdec3c8d5cc3f
Michal Margetín <86981961+SharkStudiosSK@users.noreply.github.com>
2025-02-01 08:59:28 +0100

    Add Slovak translation file and update relevant files
    * Add new translation file `sk_SK.ts` with Slovak translations for all relevant strings in the emulator.
    * Update `main_window.cpp` to include necessary headers.
    * Update `settings_dialog.h` to include necessary headers.

--- src/qt_gui/main_window.cpp
+++ src/qt_gui/main_window.cpp
@@ -1,6 +1,3 @@
-// SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
-// SPDX-License-Identifier: GPL-2.0-or-later
-
 #include <QDockWidget>
 #include <QKeyEvent>
 #include <QPlainTextEdit>
---
+++ src/qt_gui/translations/sk_SK.ts
@@ -0,0 +1,425 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sk_SK">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../about_dialog.cpp" line="12"/>
+        <source>About shadPS4</source>
+        <translation>O shadPS4</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.cpp" line="13"/>
+        <source>shadPS4 Emulator</source>
+        <translation>shadPS4 Emulátor</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.cpp" line="14"/>
+        <source>Version:</source>
+        <translation>Verzia:</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.cpp" line="15"/>
+        <source>Developed by:</source>
+        <translation>Vyvinuté:</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.cpp" line="16"/>
+        <source>Website:</source>
+        <translation>Webová stránka:</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.cpp" line="17"/>
+        <source>License:</source>
+        <translation>Licencia:</translation>
+    </message>
+</context>
+<context>
+    <name>BackgroundMusicPlayer</name>
+    <message>
+        <location filename="../background_music_player.cpp" line="12"/>
+        <source>Background Music</source>
+        <translation>Hudba na pozadí</translation>
+    </message>
+</context>
+<context>
+    <name>CheatsPatches</name>
+    <message>
+        <location filename="../cheats_patches.cpp" line="12"/>
+        <source>Cheats and Patches</source>
+        <translation>Cheaty a Záplaty</translation>
+    </message>
+    <message>
+        <location filename="../cheats_patches.cpp" line="13"/>
+        <source>Download Cheats</source>
+        <translation>Stiahnuť Cheaty</translation>
+    </message>
+    <message>
+        <location filename="../cheats_patches.cpp" line="14"/>
+        <source>Download Patches</source>
+        <translation>Stiahnuť Záplaty</translation>
+    </message>
+    <message>
+        <location filename="../cheats_patches.cpp" line="15"/>
+        <source>Save</source>
+        <translation>Uložiť</translation>
+    </message>
+</context>
+<context>
+    <name>CheckUpdate</name>
+    <message>
+        <location filename="../check_update.cpp" line="12"/>
+        <source>Check for Updates</source>
+        <translation>Skontrolovať aktualizácie</translation>
+    </message>
+    <message>
+        <location filename="../check_update.cpp" line="13"/>
+        <source>Update Available</source>
+        <translation>Aktualizácia k dispozícii</translation>
+    </message>
+    <message>
+        <location filename="../check_update.cpp" line="14"/>
+        <source>No Updates Available</source>
+        <translation>Žiadne aktualizácie nie sú k dispozícii</translation>
+    </message>
+</context>
+<context>
+    <name>CompatibilityInfo</name>
+    <message>
+        <location filename="../compatibility_info.cpp" line="12"/>
+        <source>Compatibility Information</source>
+        <translation>Informácie o kompatibilite</translation>
+    </message>
+    <message>
+        <location filename="../compatibility_info.cpp" line="13"/>
+        <source>Update Compatibility Database</source>
+        <translation>Aktualizovať databázu kompatibility</translation>
+    </message>
+</context>
+<context>
+    <name>ElfViewer</name>
+    <message>
+        <location filename="../elf_viewer.cpp" line="12"/>
+        <source>ELF Viewer</source>
+        <translation>ELF Prehliadač</translation>
+    </message>
+</context>
+<context>
+    <name>GameGridFrame</name>
+    <message>
+        <location filename="../game_grid_frame.cpp" line="12"/>
+        <source>Game Grid</source>
+        <translation>Herná mriežka</translation>
+    </message>
+</context>
+<context>
+    <name>GameInfo</name>
+    <message>
+        <location filename="../game_info.cpp" line="12"/>
+        <source>Game Information</source>
+        <translation>Informácie o hre</translation>
+    </message>
+</context>
+<context>
+    <name>GameInstallDialog</name>
+    <message>
+        <location filename="../game_install_dialog.cpp" line="12"/>
+        <source>Install Game</source>
+        <translation>Inštalovať hru</translation>
+    </message>
+</context>
+<context>
+    <name>GameListFrame</name>
+    <message>
+        <location filename="../game_list_frame.cpp" line="12"/>
+        <source>Game List</source>
+        <translation>Zoznam hier</translation>
+    </message>
+</context>
+<context>
+    <name>InstallDirSelect</name>
+    <message>
+        <location filename="../install_dir_select.cpp" line="12"/>
+        <source>Select Install Directory</source>
+        <translation>Vyberte inštalačný adresár</translation>
+    </message>
+</context>
+<context>
+    <name>KbmConfigDialog</name>
+    <message>
+        <location filename="../kbm_config_dialog.cpp" line="12"/>
+        <source>Keyboard and Mouse Configuration</source>
+        <translation>Konfigurácia klávesnice a myši</translation>
+    </message>
+</context>
+<context>
+    <name>KbmHelpDialog</name>
+    <message>
+        <location filename="../kbm_help_dialog.cpp" line="12"/>
+        <source>Keyboard and Mouse Help</source>
+        <translation>Pomoc s klávesnicou a myšou</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../main_window.cpp" line="12"/>
+        <source>Main Window</source>
+        <translation>Hlavné okno</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="13"/>
+        <source>File</source>
+        <translation>Súbor</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="14"/>
+        <source>Settings</source>
+        <translation>Nastavenia</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="15"/>
+        <source>Help</source>
+        <translation>Pomoc</translation>
+    </message>
+</context>
+<context>
+    <name>PkgViewer</name>
+    <message>
+        <location filename="../pkg_viewer.cpp" line="12"/>
+        <source>PKG Viewer</source>
+        <translation>PKG Prehliadač</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settings_dialog.cpp" line="12"/>
+        <source>Settings</source>
+        <translation>Nastavenia</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="13"/>
+        <source>General</source>
+        <translation>Všeobecné</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="14"/>
+        <source>GUI</source>
+        <translation>GUI</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="15"/>
+        <source>Graphics</source>
+        <translation>Grafika</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="16"/>
+        <source>Input</source>
+        <translation>Vstup</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="17"/>
+        <source>Paths</source>
+        <translation>Cesty</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="18"/>
+        <source>Debug</source>
+        <translation>Ladenie</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="19"/>
+        <source>Console Language</source>
+        <translation>Jazyk konzoly</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="20"/>
+        <source>Emulator Language</source>
+        <translation>Jazyk emulátora</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="21"/>
+        <source>Fullscreen</source>
+        <translation>Celá obrazovka</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="22"/>
+        <source>Separate Updates</source>
+        <translation>Oddelené aktualizácie</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="23"/>
+        <source>Show Splash Screen</source>
+        <translation>Zobraziť úvodnú obrazovku</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="24"/>
+        <source>Enable Discord RPC</source>
+        <translation>Povoliť Discord RPC</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="25"/>
+        <source>Username</source>
+        <translation>Používateľské meno</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="26"/>
+        <source>Trophy Key</source>
+        <translation>Kľúč trofeje</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="27"/>
+        <source>Log Type</source>
+        <translation>Typ záznamu</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="28"/>
+        <source>Log Filter</source>
+        <translation>Filter záznamu</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="29"/>
+        <source>Background Music</source>
+        <translation>Hudba na pozadí</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="30"/>
+        <source>Disable Trophy Popups</source>
+        <translation>Zakázať vyskakovacie okná trofejí</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="31"/>
+        <source>Enable Compatibility</source>
+        <translation>Povoliť kompatibilitu</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="32"/>
+        <source>Check Compatibility on Startup</source>
+        <translation>Skontrolovať kompatibilitu pri spustení</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="33"/>
+        <source>Update Compatibility Database</source>
+        <translation>Aktualizovať databázu kompatibility</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="34"/>
+        <source>Hide Cursor</source>
+        <translation>Skryť kurzor</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="35"/>
+        <source>Idle Timeout</source>
+        <translation>Časový limit nečinnosti</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="36"/>
+        <source>Back Button Behavior</source>
+        <translation>Správanie tlačidla späť</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="37"/>
+        <source>Graphics Adapter</source>
+        <translation>Grafický adaptér</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="38"/>
+        <source>Resolution</source>
+        <translation>Rozlíšenie</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="39"/>
+        <source>Height Divider</source>
+        <translation>Rozdeľovač výšky</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="40"/>
+        <source>Dump Shaders</source>
+        <translation>Dumpovať shadery</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="41"/>
+        <source>Null GPU</source>
+        <translation>Null GPU</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="42"/>
+        <source>Game Folders</source>
+        <translation>Herné priečinky</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="43"/>
+        <source>Add Folder</source>
+        <translation>Pridať priečinok</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="44"/>
+        <source>Remove Folder</source>
+        <translation>Odstrániť priečinok</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="45"/>
+        <source>Save Data</source>
+        <translation>Uložiť dáta</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="46"/>
+        <source>Browse</source>
+        <translation>Prehľadávať</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="47"/>
+        <source>Debug Dump</source>
+        <translation>Debug Dump</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="48"/>
+        <source>Vulkan Validation</source>
+        <translation>Vulkan Validácia</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="49"/>
+        <source>Vulkan Sync Validation</source>
+        <translation>Vulkan Sync Validácia</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="50"/>
+        <source>RenderDoc</source>
+        <translation>RenderDoc</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="51"/>
+        <source>Crash Diagnostics</source>
+        <translation>Diagnostika pádov</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="52"/>
+        <source>Guest Markers</source>
+        <translation>Guest Markers</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="53"/>
+        <source>Host Markers</source>
+        <translation>Host Markers</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="54"/>
+        <source>Copy GPU Buffers</source>
+        <translation>Kopírovať GPU Buffers</translation>
+    </message>
+    <message>
+        <location filename="../settings_dialog.cpp" line="55"/>
+        <source>Collect Shader for Debug</source>
+        <translation>Zbierať Shader pre Debug</translation>
+    </message>
+</context>
+<context>
+    <name>TrophyViewer</name>
+    <message>
+        <location filename="../trophy_viewer.cpp" line="12"/>
+        <source>Trophy Viewer</source>
+        <translation>Prehliadač trofejí</translation>
+    </message>
+</context>
+</TS>
