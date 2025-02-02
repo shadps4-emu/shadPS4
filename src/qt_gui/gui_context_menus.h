c1e73fb2f5f4113d7bb1908f2340da51502e2046
C4ndyF1sh <128715345+C4ndyF1sh@users.noreply.github.com>
2025-02-02 02:05:32 +0100

    Update gui_context_menus.h

--- src/qt_gui/gui_context_menus.h
+++ src/qt_gui/gui_context_menus.h
@@ -345,7 +345,7 @@ public:
             QClipboard* clipboard = QGuiApplication::clipboard();
             clipboard->setText(QString::fromStdString(m_games[itemID].name));
         }
-                
+
         if (selected == copySerial) {
             QClipboard* clipboard = QGuiApplication::clipboard();
             clipboard->setText(QString::fromStdString(m_games[itemID].serial));
@@ -355,7 +355,7 @@ public:
             QClipboard* clipboard = QGuiApplication::clipboard();
             clipboard->setText(QString::fromStdString(m_games[itemID].version));
         }
-                
+   
         if (selected == copySize) {
             QClipboard* clipboard = QGuiApplication::clipboard();
             clipboard->setText(QString::fromStdString(m_games[itemID].size));
