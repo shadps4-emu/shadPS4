c5cd4bc5cfade900bc4853a0e1a57b7032a5afd9
hspir404 <spillbot1910@gmail.com>
2025-02-03 10:37:11 +0000

    Remove log line that was consuming as much as 0.6ms frame time (#2335)

--- src/core/libraries/kernel/process.cpp
+++ src/core/libraries/kernel/process.cpp
@@ -13,7 +13,6 @@
 namespace Libraries::Kernel {
 
 int PS4_SYSV_ABI sceKernelIsNeoMode() {
-    LOG_DEBUG(Kernel_Sce, "called");
     return Config::isNeoModeConsole() &&
            Common::ElfInfo::Instance().GetPSFAttributes().support_neo_mode;
 }
