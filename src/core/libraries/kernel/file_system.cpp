b29a1ec397a44c0c5e4c38554ddacb40bfad8aab
mailwl <mailwl@gmail.com>
2025-02-07 21:52:45 +0300

    refactoring

--- src/core/libraries/kernel/file_system.cpp
+++ src/core/libraries/kernel/file_system.cpp
@@ -320,12 +320,11 @@ s64 PS4_SYSV_ABI posix_lseek(int d, s64 offset, int whence) {
     s64 result = sceKernelLseek(d, offset, whence);
     if (result < 0) {
         LOG_ERROR(Kernel_Pthread, "posix_lseek: error = {}", result);
+        ErrSceToPosix(result);
         // Workaround for XNA AudioEngine
         if (whence == SEEK_SET && (s32)offset == -1) {
-            SetPosixErrno(EINVAL);
             return offset;
         }
-        ErrSceToPosix(result);
         return -1;
     }
     return result;
