c9d425dc08a3b061446986869d256152517c6273
squidbus <175574877+squidbus@users.noreply.github.com>
2025-02-12 17:53:42 -0800

    fix: Correct number of allocated VGPRs.

--- src/video_core/amdgpu/liverpool.h
+++ src/video_core/amdgpu/liverpool.h
@@ -143,6 +143,11 @@ struct Liverpool {
             const u32 num_dwords = bininfo.length / sizeof(u32);
             return std::span{code, num_dwords};
         }
+
+        [[nodiscard]] u32 NumVgprs() const {
+            // Each increment allocates 4 registers, where 0 = 4 registers.
+            return (settings.num_vgprs + 1) * 4;
+        }
     };
 
     struct HsTessFactorClamp {
