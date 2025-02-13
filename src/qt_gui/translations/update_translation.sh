d8f1931938139aaf5b7fecc616012806d8d5def2
DanielSvoboda <daniel.svoboda@hotmail.com>
2025-02-13 01:41:04 -0300

    Update update_translation.sh

--- src/qt_gui/translations/update_translation.sh
+++ src/qt_gui/translations/update_translation.sh
@@ -3,20 +3,11 @@
 SCRIPTDIR=$(dirname "${BASH_SOURCE[0]}")
 
 OPTS="-tr-function-alias QT_TRANSLATE_NOOP+=TRANSLATE,QT_TRANSLATE_NOOP+=TRANSLATE_SV,QT_TRANSLATE_NOOP+=TRANSLATE_STR,QT_TRANSLATE_NOOP+=TRANSLATE_FS,QT_TRANSLATE_N_NOOP3+=TRANSLATE_FMT,QT_TRANSLATE_NOOP+=TRANSLATE_NOOP,translate+=TRANSLATE_PLURAL_STR,translate+=TRANSLATE_PLURAL_FS -no-obsolete"
-
-SRCDIRS=$(realpath "$SCRIPTDIR/..")/\ $(realpath "$SCRIPTDIR/../..")/
-
+SRCDIRS=$(realpath "$SCRIPTDIR/..") $(realpath "$SCRIPTDIR/../..")
 OUTDIR=$(realpath "$SCRIPTDIR")
 
-FILES=$(find $SRCDIRS -type f -name "*.ts")
+lupdate $SRCDIRS $OPTS -locations none -source-language en -ts "$OUTDIR/en_US.ts"
 
-for FILE in $FILES; do
-    FILENAME=$(basename "$FILE" .ts)
-
-    echo "Updating translation for language: $FILENAME"
-    lupdate $SRCDIRS $OPTS -locations none -source-language $FILENAME -ts "$OUTDIR/$FILENAME.ts"
-
-    if ! head -n 2 "$OUTDIR/$FILENAME.ts" | grep -q "SPDX-FileCopyrightText"; then
-        sed -i '2i\<!-- SPDX-FileCopyrightText: Copyright 2025 shadPS4 Emulator Project\n     SPDX-License-Identifier: GPL-2.0-or-later -->' "$OUTDIR/$FILENAME.ts"
-    fi
-done
+if ! head -n 2 "$OUTDIR/en_US.ts" | grep -q "SPDX-FileCopyrightText"; then
+    sed -i '2i\<!-- SPDX-FileCopyrightText: Copyright 2025 shadPS4 Emulator Project\n     SPDX-License-Identifier: GPL-2.0-or-later -->' "$OUTDIR/en_US.ts"
+fi
