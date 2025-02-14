7bd817fd462b4a2c7f35066697e5bb1dc17991d4
Dmugetsu <diegoavilafacebook75@gmail.com>
2025-02-14 04:33:45 -0600

    Adding KBM icon for kbm remaps.

--- src/qt_gui/main_window_ui.h
+++ src/qt_gui/main_window_ui.h
@@ -47,6 +47,7 @@ public:
     QPushButton* refreshButton;
     QPushButton* settingsButton;
     QPushButton* controllerButton;
+    QPushButton* keyboardButton;
 
     QWidget* sizeSliderContainer;
     QHBoxLayout* sizeSliderContainer_layout;
@@ -210,6 +211,10 @@ public:
         controllerButton->setFlat(true);
         controllerButton->setIcon(QIcon(":images/controller_icon.png"));
         controllerButton->setIconSize(QSize(40, 40));
+        keyboardButton = new QPushButton(centralWidget);
+        keyboardButton->setFlat(true);
+        keyboardButton->setIcon(QIcon(":images/keyboard_icon.png"));
+        keyboardButton->setIconSize(QSize(40, 40));
 
         sizeSliderContainer = new QWidget(centralWidget);
         sizeSliderContainer->setObjectName("sizeSliderContainer");
