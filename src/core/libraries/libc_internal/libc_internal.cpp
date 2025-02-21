f758982964b263df6778aeb7625aae84d6e50136
georgemoralis <giorgosmrls@gmail.com>
2025-02-21 18:44:05 +0200

    clang fix

--- src/core/libraries/libc_internal/libc_internal.cpp
+++ src/core/libraries/libc_internal/libc_internal.cpp
@@ -1,9 +1,9 @@
 // SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-//#include <cmath>
-//#include <csetjmp>
-//#include <string>
+// #include <cmath>
+// #include <csetjmp>
+// #include <string>
 
 #include "common/assert.h"
 #include "common/logging/log.h"
@@ -11,20 +11,14 @@
 #include "core/libraries/libs.h"
 #include "libc_internal.h"
 #include "libc_internal_math.h"
-#include "libc_internal_str.h"
 #include "libc_internal_memory.h"
+#include "libc_internal_str.h"
 
 namespace Libraries::LibcInternal {
-
-
-
-
 
 void RegisterlibSceLibcInternal(Core::Loader::SymbolsResolver* sym) {
     RegisterlibSceLibcInternalMath(sym);
     RegisterlibSceLibcInternalStr(sym);
     RegisterlibSceLibcInternalMemory(sym);
-
-
 }
 } // namespace Libraries::LibcInternal
