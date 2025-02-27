ac515a2bb87981234be080585b5a67025a46f537
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2025-02-27 14:02:03 +0100

    Multiple controllers v0 Contains: - replacing a whole lot of things that were just a global with a 4 long array global - implementing sceUserServiceGetEvent to handle users connecting/disconnecting (only half done, currently you need to specify the number of players from config - Even more shit added to remapping, this time a new field denoting the controller ID for the input or output (for inputs it only concerns gamepads, as those are the only ones that you can connect more than one of) - reverts ngoquang's engine, as it's currently unused, and I didn't want to deal with yet another system that had to be updated

--- src/sdl_window.h
+++ src/sdl_window.h
@@ -15,25 +15,8 @@ struct SDL_Gamepad;
 union SDL_Event;
 
 namespace Input {
-
-class SDLInputEngine : public Engine {
-public:
-    ~SDLInputEngine() override;
-    void Init() override;
-    void SetLightBarRGB(u8 r, u8 g, u8 b) override;
-    void SetVibration(u8 smallMotor, u8 largeMotor) override;
-    float GetGyroPollRate() const override;
-    float GetAccelPollRate() const override;
-    State ReadState() override;
-
-private:
-    SDL_Gamepad* m_gamepad = nullptr;
-
-    float m_gyro_poll_rate = 0.0f;
-    float m_accel_poll_rate = 0.0f;
-};
-
-} // namespace Input
+class GameController;
+}
 
 namespace Frontend {
 
@@ -65,7 +48,7 @@ class WindowSDL {
     int keyboard_grab = 0;
 
 public:
-    explicit WindowSDL(s32 width, s32 height, Input::GameController* controller,
+    explicit WindowSDL(s32 width, s32 height, Input::GameControllers* controllers,
                        std::string_view window_title);
     ~WindowSDL();
 
@@ -103,7 +86,7 @@ private:
 private:
     s32 width;
     s32 height;
-    Input::GameController* controller;
+    Input::GameControllers controllers{};
     WindowSystemInfo window_info{};
     SDL_Window* window{};
     bool is_shown{};
