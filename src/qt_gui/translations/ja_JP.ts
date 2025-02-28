f891548050eba2dc86f1123230392fc32c2d72a0
georgemoralis <giorgosmrls@gmail.com>
2025-02-28 17:53:28 +0200

    New translations en_us.ts (Japanese)

--- src/qt_gui/translations/ja_JP.ts
+++ src/qt_gui/translations/ja_JP.ts
@@ -856,6 +856,10 @@
       <translation>このゲームにはアップデートフォルダがありません！</translation>
     </message>
     <message>
+      <source>No log file found for this game!</source>
+      <translation type="unfinished">No log file found for this game!</translation>
+    </message>
+    <message>
       <source>Failed to convert icon.</source>
       <translation>アイコンの変換に失敗しました。</translation>
     </message>
