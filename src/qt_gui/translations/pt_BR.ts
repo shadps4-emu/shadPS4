a1bc679d935809ccd8d1d98f5ff5d120e435e8e0
georgemoralis <giorgosmrls@gmail.com>
2025-02-28 17:53:14 +0200

    New translations en_us.ts (Portuguese, Brazilian)

--- src/qt_gui/translations/pt_BR.ts
+++ src/qt_gui/translations/pt_BR.ts
@@ -856,6 +856,10 @@
       <translation>Este jogo não possui pasta de atualização para abrir!</translation>
     </message>
     <message>
+      <source>No log file found for this game!</source>
+      <translation type="unfinished">No log file found for this game!</translation>
+    </message>
+    <message>
       <source>Failed to convert icon.</source>
       <translation>Falha ao converter o ícone.</translation>
     </message>
