2fd43d418f09e7c1fd71c06a8ec01dd06438df7c
georgemoralis <giorgosmrls@gmail.com>
2025-02-28 17:53:11 +0200

    New translations en_us.ts (Albanian)

--- src/qt_gui/translations/sq_AL.ts
+++ src/qt_gui/translations/sq_AL.ts
@@ -856,6 +856,10 @@
       <translation>Kjo lojë nuk ka dosje përditësimi për të hapur!</translation>
     </message>
     <message>
+      <source>No log file found for this game!</source>
+      <translation type="unfinished">No log file found for this game!</translation>
+    </message>
+    <message>
       <source>Failed to convert icon.</source>
       <translation>Konvertimi i ikonës dështoi.</translation>
     </message>
