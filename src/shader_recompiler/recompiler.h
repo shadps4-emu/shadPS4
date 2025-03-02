0f6912cf189b7b4bc61a9d8d5ec267e9bebd5bcd
Lander Gallastegi <landergallastegi@gmail.com>
2025-03-02 23:55:19 +0100

    Subprogram creation

--- src/shader_recompiler/recompiler.h
+++ src/shader_recompiler/recompiler.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#include "common/object_pool.h"
+#include "shader_recompiler/pools.h"
 #include "shader_recompiler/ir/basic_block.h"
 #include "shader_recompiler/ir/program.h"
 
@@ -11,21 +11,6 @@ namespace Shader {
 
 struct Profile;
 struct RuntimeInfo;
-
-struct Pools {
-    static constexpr u32 InstPoolSize = 8192;
-    static constexpr u32 BlockPoolSize = 32;
-
-    Common::ObjectPool<IR::Inst> inst_pool;
-    Common::ObjectPool<IR::Block> block_pool;
-
-    explicit Pools() : inst_pool{InstPoolSize}, block_pool{BlockPoolSize} {}
-
-    void ReleaseContents() {
-        inst_pool.ReleaseContents();
-        block_pool.ReleaseContents();
-    }
-};
 
 [[nodiscard]] IR::Program TranslateProgram(std::span<const u32> code, Pools& pools, Info& info,
                                            RuntimeInfo& runtime_info, const Profile& profile);
