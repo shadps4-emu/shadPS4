f93de386bd3b6332de1f53a5e0acaf639e1bded2
Dmugetsu <diegoavilafacebook75@gmail.com>
2025-03-23 12:00:17 -0600

    Proper fix for Resume icon

--- src/qt_gui/main_window.cpp
+++ src/qt_gui/main_window.cpp
@@ -243,8 +243,6 @@ void MainWindow::UpdateToolbarButtons() {
     ui->playButton->setVisible(false);
     ui->pauseButton->setVisible(true);
 
-    bool isLightTheme = isIconBlack;
-
     if (showLabels) {
         QLabel* playButtonLabel = ui->playButton->parentWidget()->findChild<QLabel*>();
         if (playButtonLabel)
@@ -255,7 +253,7 @@ void MainWindow::UpdateToolbarButtons() {
         ui->pauseButton->setIcon(ui->playButton->icon());
         ui->pauseButton->setToolTip(tr("Resume"));
     } else {
-        if (isLightTheme) {
+        if (isIconBlack) {
             ui->pauseButton->setIcon(QIcon(":images/pause_icon.png"));
         } else {
             ui->pauseButton->setIcon(RecolorIcon(QIcon(":images/pause_icon.png"), isWhite));
