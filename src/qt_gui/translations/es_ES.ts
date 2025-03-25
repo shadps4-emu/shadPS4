7fbc17c319f76c2f4742afc986f43259106ecace
georgemoralis <giorgosmrls@gmail.com>
2025-03-25 13:51:36 +0200

    New translations en_us.ts (Spanish)

--- src/qt_gui/translations/es_ES.ts
+++ src/qt_gui/translations/es_ES.ts
@@ -1308,11 +1308,11 @@
     </message>
     <message>
       <source>Trophy Viewer</source>
-      <translation type="unfinished">Trophy Viewer</translation>
+      <translation>Expositor de Trofeos</translation>
     </message>
     <message>
       <source>No games found. Please add your games to your library first.</source>
-      <translation type="unfinished">No games found. Please add your games to your library first.</translation>
+      <translation>No se encontraron juegos. Por favor, añade tus juegos a tu biblioteca primero.</translation>
     </message>
     <message>
       <source>PKG Viewer</source>
@@ -2201,7 +2201,7 @@
     </message>
     <message>
       <source>Select Game:</source>
-      <translation type="unfinished">Select Game:</translation>
+      <translation>Selecciona un Juego:</translation>
     </message>
     <message>
       <source>Progress</source>
