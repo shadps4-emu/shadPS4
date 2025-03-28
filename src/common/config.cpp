3c161ba69514600fad5d1818b88c8753062f5f4e
squidbus <175574877+squidbus@users.noreply.github.com>
2025-03-28 10:08:44 -0700

    misc: Remove dead code.

--- src/common/config.cpp
+++ src/common/config.cpp
@@ -98,7 +98,6 @@ u32 m_slider_pos_grid = 0;
 u32 m_table_mode = 0;
 u32 m_window_size_W = 1280;
 u32 m_window_size_H = 720;
-std::vector<std::string> m_pkg_viewer;
 std::vector<std::string> m_elf_viewer;
 std::vector<std::string> m_recent_files;
 std::string emulator_language = "en_US";
@@ -601,11 +600,6 @@ void setMainWindowHeight(u32 height) {
     m_window_size_H = height;
 }
 
-void setPkgViewer(const std::vector<std::string>& pkgList) {
-    m_pkg_viewer.resize(pkgList.size());
-    m_pkg_viewer = pkgList;
-}
-
 void setElfViewer(const std::vector<std::string>& elfList) {
     m_elf_viewer.resize(elfList.size());
     m_elf_viewer = elfList;
@@ -707,10 +701,6 @@ u32 getMainWindowWidth() {
 
 u32 getMainWindowHeight() {
     return m_window_size_H;
-}
-
-std::vector<std::string> getPkgViewer() {
-    return m_pkg_viewer;
 }
 
 std::vector<std::string> getElfViewer() {
@@ -886,7 +876,6 @@ void load(const std::filesystem::path& path) {
         main_window_geometry_y = toml::find_or<int>(gui, "geometry_y", 0);
         main_window_geometry_w = toml::find_or<int>(gui, "geometry_w", 0);
         main_window_geometry_h = toml::find_or<int>(gui, "geometry_h", 0);
-        m_pkg_viewer = toml::find_or<std::vector<std::string>>(gui, "pkgDirs", {});
         m_elf_viewer = toml::find_or<std::vector<std::string>>(gui, "elfDirs", {});
         m_recent_files = toml::find_or<std::vector<std::string>>(gui, "recentFiles", {});
         m_table_mode = toml::find_or<int>(gui, "gameTableMode", 0);
@@ -1105,7 +1094,6 @@ void saveMainWindow(const std::filesystem::path& path) {
     data["GUI"]["geometry_y"] = main_window_geometry_y;
     data["GUI"]["geometry_w"] = main_window_geometry_w;
     data["GUI"]["geometry_h"] = main_window_geometry_h;
-    data["GUI"]["pkgDirs"] = m_pkg_viewer;
     data["GUI"]["elfDirs"] = m_elf_viewer;
     data["GUI"]["recentFiles"] = m_recent_files;
 
