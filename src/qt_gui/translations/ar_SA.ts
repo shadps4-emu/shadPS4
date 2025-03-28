5a93c9e41a901822fd87a74dc7972578e7130846
georgemoralis <giorgosmrls@gmail.com>
2025-03-28 19:47:47 +0200

    New translations en_us.ts (Arabic)

--- src/qt_gui/translations/ar_SA.ts
+++ src/qt_gui/translations/ar_SA.ts
@@ -1516,43 +1516,43 @@
     </message>
     <message>
       <source>Play</source>
-      <translation type="unfinished">Play</translation>
+      <translation>أبدأ اللعب</translation>
     </message>
     <message>
       <source>Pause</source>
-      <translation type="unfinished">Pause</translation>
+      <translation>توقف مؤقت</translation>
     </message>
     <message>
       <source>Stop</source>
-      <translation type="unfinished">Stop</translation>
+      <translation>إيقاف</translation>
     </message>
     <message>
       <source>Restart</source>
-      <translation type="unfinished">Restart</translation>
+      <translation>إعادة تشغيل</translation>
     </message>
     <message>
       <source>Full Screen</source>
-      <translation type="unfinished">Full Screen</translation>
+      <translation>وضع ملء الشاشة</translation>
     </message>
     <message>
       <source>Controllers</source>
-      <translation type="unfinished">Controllers</translation>
+      <translation>أذرعة التحكم</translation>
     </message>
     <message>
       <source>Keyboard</source>
-      <translation type="unfinished">Keyboard</translation>
+      <translation>لوحة المفاتيح</translation>
     </message>
     <message>
       <source>Refresh List</source>
-      <translation type="unfinished">Refresh List</translation>
+      <translation>تحديث القائمة</translation>
     </message>
     <message>
       <source>Resume</source>
-      <translation type="unfinished">Resume</translation>
+      <translation>استئناف</translation>
     </message>
     <message>
       <source>Show Labels Under Icons</source>
-      <translation type="unfinished">Show Labels Under Icons</translation>
+      <translation>إظهار العلامات أسفل الأيقونات</translation>
     </message>
   </context>
   <context>
