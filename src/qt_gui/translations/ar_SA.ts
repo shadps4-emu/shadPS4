9ff57ad689c5d9ee575e1cd522db9fa46ee5bdcc
georgemoralis <giorgosmrls@gmail.com>
2025-03-30 01:39:49 +0200

    New translations en_us.ts (Arabic)

--- src/qt_gui/translations/ar_SA.ts
+++ src/qt_gui/translations/ar_SA.ts
@@ -1475,10 +1475,6 @@
       <translation>المحاكي</translation>
     </message>
     <message>
-      <source>Enable Separate Update Folder</source>
-      <translation type="unfinished">Enable Separate Update Folder</translation>
-    </message>
-    <message>
       <source>Default tab when opening settings</source>
       <translation>علامة التبويب الافتراضية عند فتح الإعدادات</translation>
     </message>
@@ -1741,10 +1737,6 @@
     <message>
       <source>Emulator Language:\nSets the language of the emulator&apos;s user interface.</source>
       <translation>لغة المحاكي:\nتحدد لغة واجهة المستخدم الخاصة بالمحاكي.</translation>
-    </message>
-    <message>
-      <source>Enable Separate Update Folder:\nEnables installing game updates into a separate folder for easy management.\nThis can be manually created by adding the extracted update to the game folder with the name &quot;CUSA00000-UPDATE&quot; where the CUSA ID matches the game&apos;s ID.</source>
-      <translation>Enable Separate Update Folder:\nEnables installing game updates into a separate folder for easy management.</translation>
     </message>
     <message>
       <source>Show Splash Screen:\nShows the game&apos;s splash screen (a special image) while the game is starting.</source>
