5d4b94cf0a15aa86b8e2bb23f8555e65464ac56f
Dmugetsu <diegoavilafacebook75@gmail.com>
2025-04-26 11:02:43 -0600

    more fixes

--- cmake/Findlibusb.cmake
+++ cmake/Findlibusb.cmake
@@ -1,13 +1,16 @@
-# SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
+# SPDX-FileCopyrightText: Copyright 2025 shadPS4 Emulator Project
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 find_path(LIBUSB_INCLUDE_DIR NAMES libusb.h)
 find_library(LIBUSB_LIBRARY NAMES usb-1.0.27)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Libusb DEFAULT_MSG LIBUSB_LIBRARY LIBUSB_INCLUDE_DIR)
+find_package_handle_standard_args(libusb
+    REQUIRED_VARS LIBUSB_LINK_LIBRARIES
+    VERSION_VAR LIBUSB_VERSION
+)
 
-if(LIBUSB_FOUND)
-  set(LIBUSB_INCLUDE_DIRS ${LIBUSB_INCLUDE_DIR})
-  set(LIBUSB_LIBRARIES ${LIBUSB_LIBRARY})
+if (libusb_FOUND AND NOT TARGET libusb::usb)
+    add_library(libusb::usb ALIAS PkgConfig::LIBUSB)
+
 endif()
