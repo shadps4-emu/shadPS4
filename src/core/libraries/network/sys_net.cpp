07a8f3d650b3c25ff8903fe892132331a5389bcd
georgemoralis <giorgosmrls@gmail.com>
2025-04-27 11:03:42 +0300

    logging

--- src/core/libraries/network/sys_net.cpp
+++ src/core/libraries/network/sys_net.cpp
@@ -1,59 +1,75 @@
 // SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include <common/logging/log.h>
 #include "sys_net.h"
 
 namespace Libraries::Net {
 
 int PS4_SYSV_ABI sys_connect(OrbisNetId s, const OrbisNetSockaddr* addr, u32 addrlen) {
+    LOG_ERROR(Lib_Net, "(STUBBED) called");
     return -1;
 }
 int PS4_SYSV_ABI sys_bind(OrbisNetId s, const OrbisNetSockaddr* addr, u32 addrlen) {
+    LOG_ERROR(Lib_Net, "(STUBBED) called");
     return -1;
 }
 int PS4_SYSV_ABI sys_accept(OrbisNetId s, const OrbisNetSockaddr* addr, u32* paddrlen) {
+    LOG_ERROR(Lib_Net, "(STUBBED) called");
     return -1;
 }
 int PS4_SYSV_ABI sys_getpeername(OrbisNetId s, const OrbisNetSockaddr* addr, u32* paddrlen) {
+    LOG_ERROR(Lib_Net, "(STUBBED) called");
     return -1;
 }
 int PS4_SYSV_ABI sys_getsockname(OrbisNetId s, const OrbisNetSockaddr* addr, u32* paddrlen) {
+    LOG_ERROR(Lib_Net, "(STUBBED) called");
     return -1;
 }
 int PS4_SYSV_ABI sys_getsockopt(OrbisNetId s, int level, int optname, void* optval, u32* optlen) {
+    LOG_ERROR(Lib_Net, "(STUBBED) called");
     return -1;
 }
 int PS4_SYSV_ABI sys_listen(OrbisNetId s, int backlog) {
+    LOG_ERROR(Lib_Net, "(STUBBED) called");
     return -1;
 }
 int PS4_SYSV_ABI sys_setsockopt(OrbisNetId s, int level, int optname, const void* optval,
                                 u32 optlen) {
+    LOG_ERROR(Lib_Net, "(STUBBED) called");
     return -1;
 }
 int PS4_SYSV_ABI sys_shutdown(OrbisNetId s, int how) {
     return -1;
 }
 int PS4_SYSV_ABI sys_socketex(const char* name, int family, int type, int protocol) {
+    LOG_ERROR(Lib_Net, "(STUBBED) called");
     return -1;
 }
 int PS4_SYSV_ABI sys_netabort(OrbisNetId s, int flags) {
+    LOG_ERROR(Lib_Net, "(STUBBED) called");
     return -1;
 }
 int PS4_SYSV_ABI sys_socketclose(OrbisNetId s) {
+    LOG_ERROR(Lib_Net, "(STUBBED) called");
     return -1;
 }
 int PS4_SYSV_ABI sys_sendto(OrbisNetId s, const void* buf, u64 len, int flags,
                             const OrbisNetSockaddr* addr, u32 addrlen) {
+    LOG_ERROR(Lib_Net, "(STUBBED) called");
     return -1;
 }
 int PS4_SYSV_ABI sys_sendmsg(OrbisNetId s, const OrbisNetMsghdr* msg, int flags) {
+    LOG_ERROR(Lib_Net, "(STUBBED) called");
     return -1;
 }
 int PS4_SYSV_ABI sys_recvfrom(OrbisNetId s, void* buf, u64 len, int flags, OrbisNetSockaddr* addr,
                               u32* paddrlen) {
+    LOG_ERROR(Lib_Net, "(STUBBED) called");
     return -1;
 }
 int PS4_SYSV_ABI sys_recvmsg(OrbisNetId s, OrbisNetMsghdr* msg, int flags) {
+    LOG_ERROR(Lib_Net, "(STUBBED) called");
     return -1;
 }
 } // namespace Libraries::Net
