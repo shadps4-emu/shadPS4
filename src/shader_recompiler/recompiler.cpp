81fa9b7fff603a188fc235373b2933962292ae9a
squidbus <175574877+squidbus@users.noreply.github.com>
2025-04-28 00:04:16 -0700

    shader_recompiler: Add lowering pass for when 64-bit float is unsupported. (#2858)
    * shader_recompiler: Add lowering pass for when 64-bit float is unsupported.
    
    * shader_recompiler: Fix PackDouble2x32/UnpackDouble2x32 type.
    
    * shader_recompiler: Remove extra bit cast implementations.

--- src/shader_recompiler/recompiler.cpp
+++ src/shader_recompiler/recompiler.cpp
@@ -60,6 +60,9 @@ IR::Program TranslateProgram(std::span<const u32> code, Pools& pools, Info& info
     program.post_order_blocks = Shader::IR::PostOrder(program.syntax_list.front());
 
     // Run optimization passes
+    if (!profile.support_float64) {
+        Shader::Optimization::LowerFp64ToFp32(program);
+    }
     Shader::Optimization::SsaRewritePass(program.post_order_blocks);
     Shader::Optimization::ConstantPropagationPass(program.post_order_blocks);
     Shader::Optimization::IdentityRemovalPass(program.blocks);
