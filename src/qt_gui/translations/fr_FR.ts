4b64032f6f8719260a456a3fcbdd0a2e6ba4e03d
georgemoralis <giorgosmrls@gmail.com>
2025-05-19 11:17:35 +0300

    New translations en_us.ts (French)

--- src/qt_gui/translations/fr_FR.ts
+++ src/qt_gui/translations/fr_FR.ts
@@ -2048,7 +2048,7 @@
     </message>
     <message>
       <source> * Unsupported Vulkan Version</source>
-      <translation type="unfinished"> * Unsupported Vulkan Version</translation>
+      <translation> * Version de Vulkan non prise en charge</translation>
     </message>
   </context>
   <context>
