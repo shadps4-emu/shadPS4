1aa39e23c03905510a2f9ca9a74631e745ca2513
georgemoralis <giorgosmrls@gmail.com>
2025-06-12 11:59:46 +0300

    fixup2

--- src/main.cpp
+++ src/main.cpp
@@ -32,107 +32,109 @@ int main(int argc, char* argv[]) {
     std::vector<std::string> game_args{};
 
     // Map of argument strings to lambda functions
-    std::unordered_map<std::string, std::function<void(int&)>> arg_map =
-    { {"-h",
-       [&](int&) {
-           std::cout << "Usage: shadps4 [options] <elf or eboot.bin path>\n"
-                        "Options:\n"
-                        "  -g, --game <path|ID>          Specify game path to launch\n"
-                        " -- ...                         Parameters passed to the game ELF. "
-                        "Needs to be at the end of the line, and everything after \"--\" is a "
-                        "game argument.\n"
-                        "  -p, --patch <patch_file>      Apply specified patch file\n"
-                        "  -i, --ignore-game-patch       Disable automatic loading of game patch\n"
-                        "  -f, --fullscreen <true|false> Specify window initial fullscreen "
-                        "state. Does not overwrite the config file.\n"
-                        "  --add-game-folder <folder>    Adds a new game folder to the config.\n"
-                        "  --set-addon-folder <folder>   Sets the addon folder to the config.\n"
-                        "  -h, --help                    Display this help message\n";
-           exit(0);
-       }},
-      {"--help", [&](int& i) { arg_map["-h"](i); }},
+    std::unordered_map<std::string, std::function<void(int&)>> arg_map = {
+        {"-h",
+         [&](int&) {
+             std::cout
+                 << "Usage: shadps4 [options] <elf or eboot.bin path>\n"
+                    "Options:\n"
+                    "  -g, --game <path|ID>          Specify game path to launch\n"
+                    " -- ...                         Parameters passed to the game ELF. "
+                    "Needs to be at the end of the line, and everything after \"--\" is a "
+                    "game argument.\n"
+                    "  -p, --patch <patch_file>      Apply specified patch file\n"
+                    "  -i, --ignore-game-patch       Disable automatic loading of game patch\n"
+                    "  -f, --fullscreen <true|false> Specify window initial fullscreen "
+                    "state. Does not overwrite the config file.\n"
+                    "  --add-game-folder <folder>    Adds a new game folder to the config.\n"
+                    "  --set-addon-folder <folder>   Sets the addon folder to the config.\n"
+                    "  -h, --help                    Display this help message\n";
+             exit(0);
+         }},
+        {"--help", [&](int& i) { arg_map["-h"](i); }},
 
-      {"-g",
-       [&](int& i) {
-           if (i + 1 < argc) {
-               game_path = argv[++i];
-               has_game_argument = true;
-           } else {
-               std::cerr << "Error: Missing argument for -g/--game\n";
-               exit(1);
-           }
-       }},
-      {"--game", [&](int& i) { arg_map["-g"](i); }},
+        {"-g",
+         [&](int& i) {
+             if (i + 1 < argc) {
+                 game_path = argv[++i];
+                 has_game_argument = true;
+             } else {
+                 std::cerr << "Error: Missing argument for -g/--game\n";
+                 exit(1);
+             }
+         }},
+        {"--game", [&](int& i) { arg_map["-g"](i); }},
 
-      {"-p",
-       [&](int& i) {
-           if (i + 1 < argc) {
-               MemoryPatcher::patchFile = argv[++i];
-           } else {
-               std::cerr << "Error: Missing argument for -p/--patch\n";
-               exit(1);
-           }
-       }},
-      {"--patch", [&](int& i) { arg_map["-p"](i); }},
-      {"-i", [&](int&) { Core::FileSys::MntPoints::ignore_game_patches = true; }},
-      {"--ignore-game-patch", [&](int& i) { arg_map["-i"](i); }},
-      {"-f",
-       [&](int& i) {
-           if (++i >= argc) {
-               std::cerr << "Error: Missing argument for -f/--fullscreen\n";
-               exit(1);
-           }
-           std::string f_param(argv[i]);
-           bool is_fullscreen;
-           if (f_param == "true") {
-               is_fullscreen = true;
-           } else if (f_param == "false") {
-               is_fullscreen = false;
-           } else {
-               std::cerr << "Error: Invalid argument for -f/--fullscreen. Use 'true' or 'false'.\n";
-               exit(1);
-           }
-           // Set fullscreen mode without saving it to config file
-           Config::setIsFullscreen(is_fullscreen);
-       }},
-      {"--fullscreen", [&](int& i) { arg_map["-f"](i); }},
-      {"--add-game-folder",
-       [&](int& i) {
-           if (++i >= argc) {
-               std::cerr << "Error: Missing argument for --add-game-folder\n";
-               exit(1);
-           }
-           std::string config_dir(argv[i]);
-           std::filesystem::path config_path = std::filesystem::path(config_dir);
-           std::error_code discard;
-           if (!std::filesystem::exists(config_path, discard)) {
-               std::cerr << "Error: File does not exist: " << config_path << "\n";
-               exit(1);
-           }
+        {"-p",
+         [&](int& i) {
+             if (i + 1 < argc) {
+                 MemoryPatcher::patchFile = argv[++i];
+             } else {
+                 std::cerr << "Error: Missing argument for -p/--patch\n";
+                 exit(1);
+             }
+         }},
+        {"--patch", [&](int& i) { arg_map["-p"](i); }},
+        {"-i", [&](int&) { Core::FileSys::MntPoints::ignore_game_patches = true; }},
+        {"--ignore-game-patch", [&](int& i) { arg_map["-i"](i); }},
+        {"-f",
+         [&](int& i) {
+             if (++i >= argc) {
+                 std::cerr << "Error: Missing argument for -f/--fullscreen\n";
+                 exit(1);
+             }
+             std::string f_param(argv[i]);
+             bool is_fullscreen;
+             if (f_param == "true") {
+                 is_fullscreen = true;
+             } else if (f_param == "false") {
+                 is_fullscreen = false;
+             } else {
+                 std::cerr
+                     << "Error: Invalid argument for -f/--fullscreen. Use 'true' or 'false'.\n";
+                 exit(1);
+             }
+             // Set fullscreen mode without saving it to config file
+             Config::setIsFullscreen(is_fullscreen);
+         }},
+        {"--fullscreen", [&](int& i) { arg_map["-f"](i); }},
+        {"--add-game-folder",
+         [&](int& i) {
+             if (++i >= argc) {
+                 std::cerr << "Error: Missing argument for --add-game-folder\n";
+                 exit(1);
+             }
+             std::string config_dir(argv[i]);
+             std::filesystem::path config_path = std::filesystem::path(config_dir);
+             std::error_code discard;
+             if (!std::filesystem::exists(config_path, discard)) {
+                 std::cerr << "Error: File does not exist: " << config_path << "\n";
+                 exit(1);
+             }
 
-           Config::addGameInstallDir(config_path);
-           Config::save(Common::FS::GetUserPath(Common::FS::PathType::UserDir) / "config.toml");
-           std::cout << "Game folder successfully saved.\n";
-           exit(0);
-       },
-       {"--set-addon-folder", [&](int& i) {
-            if (++i >= argc) {
-                std::cerr << "Error: Missing argument for --add-addon-folder\n";
-                exit(1);
-            }
-            std::string config_dir(argv[i]);
-            std::filesystem::path config_path = std::filesystem::path(config_dir);
-            std::error_code discard;
-            if (!std::filesystem::exists(config_path, discard)) {
-                std::cerr << "Error: File does not exist: " << config_path << "\n";
-                exit(1);
-            }
+             Config::addGameInstallDir(config_path);
+             Config::save(Common::FS::GetUserPath(Common::FS::PathType::UserDir) / "config.toml");
+             std::cout << "Game folder successfully saved.\n";
+             exit(0);
+         }},
+        {"--set-addon-folder", [&](int& i) {
+             if (++i >= argc) {
+                 std::cerr << "Error: Missing argument for --add-addon-folder\n";
+                 exit(1);
+             }
+             std::string config_dir(argv[i]);
+             std::filesystem::path config_path = std::filesystem::path(config_dir);
+             std::error_code discard;
+             if (!std::filesystem::exists(config_path, discard)) {
+                 std::cerr << "Error: File does not exist: " << config_path << "\n";
+                 exit(1);
+             }
 
-            Config::setAddonInstallDir(config_path);
-            Config::save(Common::FS::GetUserPath(Common::FS::PathType::UserDir) / "config.toml");
-            std::cout << "Addon folder successfully saved.\n";
-            exit(0);
-        }};
+             Config::setAddonInstallDir(config_path);
+             Config::save(Common::FS::GetUserPath(Common::FS::PathType::UserDir) / "config.toml");
+             std::cout << "Addon folder successfully saved.\n";
+             exit(0);
+         }}};
 
     if (argc == 1) {
         int dummy = 0; // one does not simply pass 0 directly
