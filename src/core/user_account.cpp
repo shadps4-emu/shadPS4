7b0d6c4bf8753fd5ba2181579dd2f94cecfe0766
georgemoralis <giorgosmrls@gmail.com>
2025-06-13 19:10:42 +0300

    use json to read local file

--- src/core/user_account.cpp
+++ src/core/user_account.cpp
@@ -2,12 +2,36 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <filesystem>
+#include <common/io_file.h>
+#include <common/path_util.h>
+#include <nlohmann/json.hpp>
+#include "common/config.h"
 #include "user_account.h"
+
+using json = nlohmann::json;
 
 user_account::user_account(const std::string& user_id) {
     // Setting userId.
     m_user_id = user_id;
-    // TODO
+
+    m_user_dir = Common::FS::GetUserPathString(Common::FS::PathType::HomeDir) + "/" +
+                 Config::getDefaultUserId() + "/";
+
+    Common::FS::IOFile userfile(m_user_dir + "localuser.json", Common::FS::FileAccessMode::Read);
+    if (userfile.IsOpen()) {
+        nlohmann::json jsonfile;
+        try {
+            jsonfile = nlohmann::json::parse(userfile.ReadString(userfile.GetSize()));
+        } catch (const nlohmann::json::parse_error& e) {
+            // TODO error code
+        }
+        userfile.Close();
+        m_username = jsonfile.value("username", "shadps4");
+        if (m_username.length() > 16) // max of 16 chars allowed
+        {
+            m_username = m_username.substr(0, 16); // substring 16 only characters to display
+        }
+    }
 }
 
 std::map<u32, user_account> user_account::GetUserAccounts(const std::string& base_dir) {
