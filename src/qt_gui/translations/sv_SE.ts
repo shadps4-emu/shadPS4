1a9301510f4a934a0f0fd526c7e6e0c3da773df0
georgemoralis <giorgosmrls@gmail.com>
2025-06-23 22:00:29 +0300

    New translations en_us.ts (Swedish)

--- src/qt_gui/translations/sv_SE.ts
+++ src/qt_gui/translations/sv_SE.ts
@@ -750,7 +750,7 @@
     </message>
     <message>
       <source>Favorite</source>
-      <translation type="unfinished">Favorite</translation>
+      <translation>Favorit</translation>
     </message>
   </context>
   <context>
@@ -956,11 +956,11 @@
     </message>
     <message>
       <source>Remove from Favorites</source>
-      <translation type="unfinished">Remove from Favorites</translation>
+      <translation>Ta bort från favoriter</translation>
     </message>
     <message>
       <source>Add to Favorites</source>
-      <translation type="unfinished">Add to Favorites</translation>
+      <translation>Lägg till i favoriter</translation>
     </message>
   </context>
   <context>
