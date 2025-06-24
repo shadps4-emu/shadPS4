8e0ed1906a182ffa6d48aaa76acff4e938a2d3dd
georgemoralis <giorgosmrls@gmail.com>
2025-06-24 13:14:36 +0300

    New translations en_us.ts (Norwegian Bokmal)

--- src/qt_gui/translations/nb_NO.ts
+++ src/qt_gui/translations/nb_NO.ts
@@ -750,7 +750,7 @@
     </message>
     <message>
       <source>Favorite</source>
-      <translation type="unfinished">Favorite</translation>
+      <translation>Favoritter</translation>
     </message>
   </context>
   <context>
@@ -956,11 +956,11 @@
     </message>
     <message>
       <source>Remove from Favorites</source>
-      <translation type="unfinished">Remove from Favorites</translation>
+      <translation>Fjern fra favoritter</translation>
     </message>
     <message>
       <source>Add to Favorites</source>
-      <translation type="unfinished">Add to Favorites</translation>
+      <translation>Legg til i favoritter</translation>
     </message>
   </context>
   <context>
