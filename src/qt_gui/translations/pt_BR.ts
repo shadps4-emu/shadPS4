11ebf2a8ceba6cc089a341cbe74f77f07f57a53a
georgemoralis <giorgosmrls@gmail.com>
2025-06-25 19:00:36 +0300

    New translations en_us.ts (Portuguese, Brazilian)

--- src/qt_gui/translations/pt_BR.ts
+++ src/qt_gui/translations/pt_BR.ts
@@ -750,7 +750,7 @@
     </message>
     <message>
       <source>Favorite</source>
-      <translation type="unfinished">Favorite</translation>
+      <translation>Favorito</translation>
     </message>
   </context>
   <context>
@@ -956,11 +956,11 @@
     </message>
     <message>
       <source>Remove from Favorites</source>
-      <translation type="unfinished">Remove from Favorites</translation>
+      <translation>Remover dos Favoritos</translation>
     </message>
     <message>
       <source>Add to Favorites</source>
-      <translation type="unfinished">Add to Favorites</translation>
+      <translation>Adicionar aos Favoritos</translation>
     </message>
   </context>
   <context>
