4c9b11fb1fa3b186da5d246d12492062c980b658
georgemoralis <giorgosmrls@gmail.com>
2025-06-28 22:13:16 +0300

    New translations en_us.ts (Turkish)

--- src/qt_gui/translations/tr_TR.ts
+++ src/qt_gui/translations/tr_TR.ts
@@ -581,7 +581,7 @@
       <source>Cannot bind any unique input more than once. Duplicate inputs mapped to the following buttons:
 
 %1</source>
-      <translation type="unfinished">Cannot bind any unique input more than once. Duplicate inputs mapped to the following buttons:
+      <translation>Aynı tuş birden fazla kez atanamaz. Aşağıdaki tuşlara birden fazla giriş atanmış:
 
 %1</translation>
     </message>
