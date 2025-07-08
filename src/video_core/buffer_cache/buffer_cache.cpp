565bde7c4f8fbb976157bd32de406e91cbcbad1f
IndecisiveTurtle <47210458+raphaelthegreat@users.noreply.github.com>
2025-07-09 00:31:38 +0300

    buffer_cache: Handle inline data to flexible memory

--- src/video_core/buffer_cache/buffer_cache.cpp
+++ src/video_core/buffer_cache/buffer_cache.cpp
@@ -312,7 +312,10 @@ void BufferCache::BindIndexBuffer(u32 index_offset) {
 void BufferCache::InlineData(VAddr address, const void* value, u32 num_bytes, bool is_gds) {
     ASSERT_MSG(address % 4 == 0, "GDS offset must be dword aligned");
     if (!is_gds) {
-        ASSERT(memory->TryWriteBacking(std::bit_cast<void*>(address), value, num_bytes));
+        if (!memory->TryWriteBacking(std::bit_cast<void*>(address), value, num_bytes)) {
+            std::memcpy(std::bit_cast<void*>(address), value, num_bytes);
+            return;
+        }
         if (!IsRegionRegistered(address, num_bytes)) {
             return;
         }
