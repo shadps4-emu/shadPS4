3ce137f7deffa09deac53c1fe2c977dbea418bda
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2025-07-12 19:59:58 +0100

    Rename fields not compiling with gcc

--- src/core/libraries/network/net.cpp
+++ src/core/libraries/network/net.cpp
@@ -883,7 +883,7 @@ int PS4_SYSV_ABI sceNetEpollWait(OrbisNetId epollid, OrbisNetEpollEvent* events,
                 .data = it->second.data,
             };
             LOG_INFO(Lib_Net, "event[{}] = ( .events = {:#x}, .ident = {}, .data = {:#x} )", i,
-                     events[i].events, events[i].ident, events[i].data.u64);
+                     events[i].events, events[i].ident, events[i].data.data_u64);
         }
         return result;
     }
