00f4eeddaf080ec65c9ac6d63141577c81819449
TheTurtle <geoster3d@gmail.com>
2025-07-14 21:23:18 +0300

    renderer_vulkan: Handle more miscellaneous GPU settings (#3241)
    * renderer_vulkan: Respect provoking vertex setting
    
    * renderer_vulkan: Handle rasterization discard
    
    * renderer_vulkan: Implement logic ops
    
    * renderer_vulkan: Properly implement depth clamp and clip
    
    * renderer_vulkan: Handle line width
    
    * Fix build
    
    * vk_pipeline_cache: Don't check depth clamp without a depth buffer
    
    * liverpool: Fix line control offset
    
    * vk_pipeline_cache: Don't run search if depth clamp is disabled
    
    * vk_pipeline_cache: Allow using viewport range when it's more restrictive then depth clamp
    
    * liverpool: Disable depth clip when near and far planes have different setting
    
    * vk_graphics_pipeline: Move warning to pipeline
    
    * vk_pipeline_cache: Revert viewport check and remove log
    
    * vk_graphics_pipeline: Enable depth clamp when depth clip is disabled and extension is not supported
    
    Without the depth clip extension depth clipping is controlled by depth clamping

--- src/video_core/renderer_vulkan/vk_graphics_pipeline.h
+++ src/video_core/renderer_vulkan/vk_graphics_pipeline.h
@@ -33,22 +33,32 @@ using VertexInputs = boost::container::static_vector<T, MaxVertexBufferCount>;
 
 struct GraphicsPipelineKey {
     std::array<size_t, MaxShaderStages> stage_hashes;
+    std::array<vk::Format, MaxVertexBufferCount> vertex_buffer_formats;
+    u32 patch_control_points;
     u32 num_color_attachments;
     std::array<vk::Format, Liverpool::NumColorBuffers> color_formats;
     std::array<Shader::PsColorBuffer, Liverpool::NumColorBuffers> color_buffers;
-    vk::Format depth_format;
-    vk::Format stencil_format;
-
-    u32 num_samples;
-    u32 mrt_mask;
-    AmdGpu::PrimitiveType prim_type;
-    Liverpool::PolygonMode polygon_mode;
-    Liverpool::ClipSpace clip_space;
-    Liverpool::ColorBufferMask cb_shader_mask;
     std::array<Liverpool::BlendControl, Liverpool::NumColorBuffers> blend_controls;
     std::array<vk::ColorComponentFlags, Liverpool::NumColorBuffers> write_masks;
-    std::array<vk::Format, MaxVertexBufferCount> vertex_buffer_formats;
-    u32 patch_control_points;
+    Liverpool::ColorBufferMask cb_shader_mask;
+    Liverpool::ColorControl::LogicOp logic_op;
+    u32 num_samples;
+    u32 mrt_mask;
+    struct {
+        Liverpool::DepthBuffer::ZFormat z_format : 2;
+        Liverpool::DepthBuffer::StencilFormat stencil_format : 1;
+        u32 depth_clamp_enable : 1;
+        u32 depth_clamp_user_defined_range : 1;
+        float min_depth_clamp;
+        float max_depth_clamp;
+    };
+    struct {
+        AmdGpu::PrimitiveType prim_type : 5;
+        Liverpool::PolygonMode polygon_mode : 2;
+        Liverpool::ClipSpace clip_space : 1;
+        Liverpool::ProvokingVtxLast provoking_vtx_last : 1;
+        u32 depth_clip_enable : 1;
+    };
 
     bool operator==(const GraphicsPipelineKey& key) const noexcept {
         return std::memcmp(this, &key, sizeof(key)) == 0;
