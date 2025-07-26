a0c240a0263428e2c5a45f4347de7273aefbacf6
nickci2002 <nickci2002@gmail.com>
2025-07-25 23:16:50 -0400

    Fixing errors

--- src/common/config.cpp
+++ src/common/config.cpp
@@ -849,8 +849,7 @@ void load(const std::filesystem::path& path, bool is_game_specific) {
     if (data.contains("Output")) {
         const toml::value& output = data.at("Output");
 
-        mainOutputDevice =
-            toml::find_or<std::string>(output, "mainOutputDevice", mainOutputDevice);
+        mainOutputDevice = toml::find_or<std::string>(output, "mainOutputDevice", mainOutputDevice);
         padSpkOutputDevice =
             toml::find_or<std::string>(output, "padSpkOutputDevice", padSpkOutputDevice);
     }
