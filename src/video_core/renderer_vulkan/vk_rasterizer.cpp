07880b95f3fc888da2d6b06199699c0d52665a39
Lander Gallastegi <landergallastegi@gmail.com>
2025-08-10 01:04:38 +0200

    Layout transition

--- src/video_core/renderer_vulkan/vk_rasterizer.cpp
+++ src/video_core/renderer_vulkan/vk_rasterizer.cpp
@@ -462,8 +462,8 @@ void Rasterizer::OnSubmit() {
         buffer_cache.ProcessFaultBuffer();
     }
     texture_cache.ProcessDownloadImages();
-    texture_cache.RunGarbageCollector();
     buffer_cache.RunGarbageCollector();
+    texture_cache.RunGarbageCollector();
 }
 
 bool Rasterizer::BindResources(const Pipeline* pipeline) {
