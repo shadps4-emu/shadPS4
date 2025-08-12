e1e55d9b4c634dd831eb94a5b0e701c697cca455
Marek Ledworowski <fotelpl@gmail.com>
2025-08-13 01:14:53 +0200

    gcc or mac, idk

--- src/common/native_fs_posix.cpp
+++ src/common/native_fs_posix.cpp
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: Copyright 2025 shadPS4 Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include <format>
+
 #include <errno.h>
 #include <sys/fcntl.h>
 #include <sys/stat.h>
