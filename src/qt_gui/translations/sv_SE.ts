85f509ac69ae5e9f89517e97193e4275c7bd0281
georgemoralis <giorgosmrls@gmail.com>
2025-08-17 11:47:18 +0300

    New translations en_us.ts (Swedish)

--- src/qt_gui/translations/sv_SE.ts
+++ src/qt_gui/translations/sv_SE.ts
@@ -2178,6 +2178,14 @@
       <source>WARNING: These features are experimental and should not be enabled unless you were told to, or a game requires it. Please ask in the shadPS4 Discord server if you have any questions.</source>
       <translation>VARNING: Dessa funktioner är experimentella och bör inte aktiveras om du inte har blivit tillsagd att göra det eller om ett spel kräver det. Fråga i shadPS4s Discord-server om du har några frågor.</translation>
     </message>
+    <message>
+      <source>Enable Background Controller Input</source>
+      <translation type="unfinished">Enable Background Controller Input</translation>
+    </message>
+    <message>
+      <source>Enable Controller Background Input:\nAllow shadPS4 to detect controller inputs when the game window is not in focus.</source>
+      <translation type="unfinished">Enable Controller Background Input:\nAllow shadPS4 to detect controller inputs when the game window is not in focus.</translation>
+    </message>
   </context>
   <context>
     <name>TrophyViewer</name>
