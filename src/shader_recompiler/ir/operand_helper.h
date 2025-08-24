bc9178de76ffbb85a2779a95497a300891d1d5e9
Frodo Baggins <baggins31084@proton.me>
2025-08-24 11:27:13 -0700

    copyright

--- src/shader_recompiler/ir/operand_helper.h
+++ src/shader_recompiler/ir/operand_helper.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
+// SPDX-FileCopyrightText: Copyright 2025 shadPS4 Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 // Some helpers to get operand indices of instructions by name to make it a bit safer.
