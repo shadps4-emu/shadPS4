dcb256c930e72af7eeec5fc680c4ee3f23a9cb8b
Stephen Miller <56742918+StevenMiller123@users.noreply.github.com>
2025-08-25 20:11:35 -0500

    libSceHmdSetupDialog stubs (#3460)
    Basic stubs designed to get some VR-optional titles further.

--- src/common/logging/types.h
+++ src/common/logging/types.h
@@ -107,6 +107,7 @@ enum class Class : u8 {
     Lib_NpParty,           ///< The LibSceNpParty implementation
     Lib_Zlib,              ///< The LibSceZlib implementation.
     Lib_Hmd,               ///< The LibSceHmd implementation.
+    Lib_HmdSetupDialog,    ///< The LibSceHmdSetupDialog implementation.
     Lib_SigninDialog,      ///< The LibSigninDialog implementation.
     Lib_Camera,            ///< The LibCamera implementation.
     Lib_CompanionHttpd,    ///< The LibCompanionHttpd implementation.
