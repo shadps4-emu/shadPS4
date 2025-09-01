fa6cf2db117fa95843da26e22366e60d280c9ef6
UltraDaCat <113462733+UltraDaCat@users.noreply.github.com>
2025-09-01 15:38:23 +0200

    there was another space... game_info.h

--- src/qt_gui/game_info.h
+++ src/qt_gui/game_info.h
@@ -19,7 +19,7 @@ public:
     QVector<GameInfo> m_games;
     QVector<GameInfo> m_games_backup;
 
-    static void SceUpdateChecker(std::string sceItem, std::filesystem::path& gameItem, 
+    static void SceUpdateChecker(std::string sceItem, std::filesystem::path& gameItem,
                                  std::filesystem::path& update_folder,
                                  std::filesystem::path& patch_folder) {
         if (std::filesystem::exists(update_folder / "sce_sys" / sceItem)) {
