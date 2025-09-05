9a0f67778712ddfc453ac1ed54cda17cace3d17b
UltraDaCat <113462733+UltraDaCat@users.noreply.github.com>
2025-09-05 22:57:23 +0200

    Update driver.cpp

--- src/core/libraries/videoout/driver.cpp
+++ src/core/libraries/videoout/driver.cpp
@@ -1,4 +1,4 @@
-﻿// SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
+// SPDX-FileCopyrightText: Copyright 2025 shadPS4 Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "common/assert.h"
@@ -269,8 +269,8 @@ void VideoOutDriver::SubmitFlipInternal(VideoOutPort* port, s32 index, s64 flip_
 }
 
 void VideoOutDriver::PresentThread(std::stop_token token) {
-    static constexpr std::chrono::nanoseconds VblankPeriod{16666667};
-    const auto vblank_period = VblankPeriod / Config::vblankDiv();
+    static constexpr std::chrono::nanoseconds VblankPeriod{1000000000};
+    const auto vblank_period = VblankPeriod / Config::vblankFreq();
 
     Common::SetCurrentThreadName("shadPS4:PresentThread");
     Common::SetCurrentThreadRealtime(vblank_period);
