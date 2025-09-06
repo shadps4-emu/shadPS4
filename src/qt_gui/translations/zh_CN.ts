c06a923b66373a7437ff6abb9894dc7e32b29ff0
georgemoralis <giorgosmrls@gmail.com>
2025-09-06 19:22:45 +0300

    New Crowdin updates (#3522)
    * New translations en_us.ts (Turkish)
    
    * New translations en_us.ts (Spanish)
    
    * New translations en_us.ts (Polish)
    
    * New translations en_us.ts (Albanian)
    
    * New translations en_us.ts (Ukrainian)
    
    * New translations en_us.ts (German)
    
    * New translations en_us.ts (Greek)
    
    * New translations en_us.ts (Vietnamese)
    
    * New translations en_us.ts (Indonesian)
    
    * New translations en_us.ts (Romanian)
    
    * New translations en_us.ts (French)
    
    * New translations en_us.ts (Arabic)
    
    * New translations en_us.ts (Catalan)
    
    * New translations en_us.ts (Danish)
    
    * New translations en_us.ts (Finnish)
    
    * New translations en_us.ts (Hungarian)
    
    * New translations en_us.ts (Italian)
    
    * New translations en_us.ts (Japanese)
    
    * New translations en_us.ts (Korean)
    
    * New translations en_us.ts (Lithuanian)
    
    * New translations en_us.ts (Dutch)
    
    * New translations en_us.ts (Portuguese)
    
    * New translations en_us.ts (Russian)
    
    * New translations en_us.ts (Slovenian)
    
    * New translations en_us.ts (Swedish)
    
    * New translations en_us.ts (Chinese Simplified)
    
    * New translations en_us.ts (Chinese Traditional)
    
    * New translations en_us.ts (Portuguese, Brazilian)
    
    * New translations en_us.ts (Persian)
    
    * New translations en_us.ts (Norwegian Bokmal)
    
    * New translations en_us.ts (Serbian (Latin))
    
    * New translations en_us.ts (Urdu (Pakistan))
    
    * New translations en_us.ts (Catalan)
    
    * New translations en_us.ts (Swedish)
    
    * New translations en_us.ts (Norwegian Bokmal)
    
    * New translations en_us.ts (Swedish)
    
    * New translations en_us.ts (Russian)

--- src/qt_gui/translations/zh_CN.ts
+++ src/qt_gui/translations/zh_CN.ts
@@ -1071,6 +1071,93 @@
     </message>
   </context>
   <context>
+    <name>Hotkeys</name>
+    <message>
+      <source>Customize Hotkeys</source>
+      <translation type="unfinished">Customize Hotkeys</translation>
+    </message>
+    <message>
+      <source>Controller Hotkeys</source>
+      <translation type="unfinished">Controller Hotkeys</translation>
+    </message>
+    <message>
+      <source>Show FPS Counter</source>
+      <translation type="unfinished">Show FPS Counter</translation>
+    </message>
+    <message>
+      <source>unmapped</source>
+      <translation type="unfinished">unmapped</translation>
+    </message>
+    <message>
+      <source>Quit Emulation</source>
+      <translation type="unfinished">Quit Emulation</translation>
+    </message>
+    <message>
+      <source>Reload Button Mappings</source>
+      <translation type="unfinished">Reload Button Mappings</translation>
+    </message>
+    <message>
+      <source>Toggle Fullscreen</source>
+      <translation type="unfinished">Toggle Fullscreen</translation>
+    </message>
+    <message>
+      <source>Toggle Pause</source>
+      <translation type="unfinished">Toggle Pause</translation>
+    </message>
+    <message>
+      <source>Keyboard Hotkeys</source>
+      <translation type="unfinished">Keyboard Hotkeys</translation>
+    </message>
+    <message>
+      <source>Renderdoc Capture (for debugging only)</source>
+      <translation type="unfinished">Renderdoc Capture (for debugging only)</translation>
+    </message>
+    <message>
+      <source>Toggle Mouse to Joystick Emulation</source>
+      <translation type="unfinished">Toggle Mouse to Joystick Emulation</translation>
+    </message>
+    <message>
+      <source>Toggle Mouse to Gyro Emulation</source>
+      <translation type="unfinished">Toggle Mouse to Gyro Emulation</translation>
+    </message>
+    <message>
+      <source>Tip: Up to three simultaneous inputs can be assigned for each hotkey</source>
+      <translation type="unfinished">Tip: Up to three simultaneous inputs can be assigned for each hotkey</translation>
+    </message>
+    <message>
+      <source>Save</source>
+      <translation type="unfinished">Save</translation>
+    </message>
+    <message>
+      <source>Apply</source>
+      <translation type="unfinished">Apply</translation>
+    </message>
+    <message>
+      <source>Cancel</source>
+      <translation type="unfinished">Cancel</translation>
+    </message>
+    <message>
+      <source>Restore Defaults</source>
+      <translation type="unfinished">Restore Defaults</translation>
+    </message>
+    <message>
+      <source>Unable to Save</source>
+      <translation type="unfinished">Unable to Save</translation>
+    </message>
+    <message>
+      <source>Cannot bind any unique input more than once. Duplicate inputs mapped to the following buttons:
+
+%1</source>
+      <translation type="unfinished">Cannot bind any unique input more than once. Duplicate inputs mapped to the following buttons:
+
+%1</translation>
+    </message>
+    <message>
+      <source>Waiting for inputs</source>
+      <translation type="unfinished">Waiting for inputs</translation>
+    </message>
+  </context>
+  <context>
     <name>KBMSettings</name>
     <message>
       <source>Configure Controls</source>
@@ -2276,77 +2363,6 @@
     <message>
       <source>Show Hidden Trophies</source>
       <translation>显示隐藏奖杯</translation>
-    </message>
-  </context>
-  <context>
-    <name>hotkeys</name>
-    <message>
-      <source>Customize Hotkeys</source>
-      <translation>自定义快捷键</translation>
-    </message>
-    <message>
-      <source>Controller Hotkeys</source>
-      <translation>控制器快捷键</translation>
-    </message>
-    <message>
-      <source>Show FPS Counter</source>
-      <translation>显示FPS计数器</translation>
-    </message>
-    <message>
-      <source>unmapped</source>
-      <translation>未映射</translation>
-    </message>
-    <message>
-      <source>Stop Emulator</source>
-      <translation>停止模拟器</translation>
-    </message>
-    <message>
-      <source>Toggle Fullscreen</source>
-      <translation>全屏显示切换</translation>
-    </message>
-    <message>
-      <source>Toggle Pause</source>
-      <translation>切换暂停</translation>
-    </message>
-    <message>
-      <source>Keyboard Hotkeys</source>
-      <translation>键盘快捷键</translation>
-    </message>
-    <message>
-      <source>Show Fps Counter: F10</source>
-      <translation>显示FPS 计数器：F10</translation>
-    </message>
-    <message>
-      <source>Stop Emulator: n/a</source>
-      <translation>停止模拟器： 不可用</translation>
-    </message>
-    <message>
-      <source>Toggle Fullscreen: F11</source>
-      <translation>切换全屏： F11</translation>
-    </message>
-    <message>
-      <source>Toggle Pause: F9</source>
-      <translation>切换暂停：F9</translation>
-    </message>
-    <message>
-      <source>Tip: Up to three inputs can be assigned for each function</source>
-      <translation>提示：最多可以为每个功能分配三个输入</translation>
-    </message>
-    <message>
-      <source>Save</source>
-      <translation>保存</translation>
-    </message>
-    <message>
-      <source>Apply</source>
-      <translation>应用</translation>
-    </message>
-    <message>
-      <source>Cancel</source>
-      <translation>取消</translation>
-    </message>
-    <message>
-      <source>Press a button</source>
-      <translation>请按一个按键</translation>
     </message>
   </context>
 </TS>
