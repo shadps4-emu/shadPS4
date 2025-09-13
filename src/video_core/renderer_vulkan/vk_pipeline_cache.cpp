0bfde1fcde657ca5f98487ba28486b07738670bb
Stephen Miller <56742918+StevenMiller123@users.noreply.github.com>
2025-09-13 06:32:24 -0500

    video_core: Check DB_SHADER_CONTROL register before performing depth exports (#3588)
    The DB_SHADER_CONTROL register has several enable flags which must be set before certain depth exports are enabled.
    This commit adds logic to respect the values in this register when performing depth exports, which fixes the regression in earlier versions of KNACK.
    I've also renamed DepthBufferControl to DepthShaderControl, since that's closer to the official name for the register.

--- src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
+++ src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
@@ -169,6 +169,12 @@ const Shader::RuntimeInfo& PipelineCache::BuildRuntimeInfo(Stage stage, LogicalS
         info.fs_info.addr_flags = regs.ps_input_addr;
         info.fs_info.num_inputs = regs.num_interp;
         info.fs_info.z_export_format = regs.z_export_format;
+        u8 stencil_ref_export_enable = regs.depth_shader_control.stencil_op_val_export_enable |
+                                       regs.depth_shader_control.stencil_test_val_export_enable;
+        info.fs_info.mrtz_mask = regs.depth_shader_control.z_export_enable |
+                                 (stencil_ref_export_enable << 1) |
+                                 (regs.depth_shader_control.mask_export_enable << 2) |
+                                 (regs.depth_shader_control.coverage_to_mask_enable << 3);
         const auto& cb0_blend = regs.blend_control[0];
         if (cb0_blend.enable) {
             info.fs_info.dual_source_blending =
