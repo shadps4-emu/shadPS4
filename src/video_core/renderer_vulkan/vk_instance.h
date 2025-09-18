e04b09d29420d6e96242ccc8b9746c111828d6b8
Lander Gallastegi <landergallastegi@gmail.com>
2025-09-18 21:03:24 +0200

    Temporarily add the 2GB budget

--- src/video_core/renderer_vulkan/vk_instance.h
+++ src/video_core/renderer_vulkan/vk_instance.h
@@ -411,7 +411,8 @@ public:
 
     /// Returns the total memory budget available to the device.
     [[nodiscard]] u64 GetTotalMemoryBudget() const {
-        return total_memory_budget;
+        return 2_GB; // TODO: this is for better garbage collection testing, temporary
+        // return total_memory_budget;
     }
 
     /// Determines if a format is supported for a set of feature flags.
