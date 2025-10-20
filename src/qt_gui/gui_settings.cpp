8a5dc49c299d2d802f6d2a5dd5eecd26d28e98d9
georgemoralis <giorgosmrls@gmail.com>
2025-10-20 14:20:50 +0300

    removed qt dir
    # Conflicts:
    #	src/qt_gui/check_update.cpp
    #	src/qt_gui/translations/ar_SA.ts
    #	src/qt_gui/translations/ca_ES.ts
    #	src/qt_gui/translations/da_DK.ts
    #	src/qt_gui/translations/de_DE.ts
    #	src/qt_gui/translations/el_GR.ts
    #	src/qt_gui/translations/en_US.ts
    #	src/qt_gui/translations/es_ES.ts
    #	src/qt_gui/translations/fa_IR.ts
    #	src/qt_gui/translations/fi_FI.ts
    #	src/qt_gui/translations/fr_FR.ts
    #	src/qt_gui/translations/hu_HU.ts
    #	src/qt_gui/translations/id_ID.ts
    #	src/qt_gui/translations/it_IT.ts
    #	src/qt_gui/translations/ja_JP.ts
    #	src/qt_gui/translations/ko_KR.ts
    #	src/qt_gui/translations/lt_LT.ts
    #	src/qt_gui/translations/nb_NO.ts
    #	src/qt_gui/translations/nl_NL.ts
    #	src/qt_gui/translations/pl_PL.ts
    #	src/qt_gui/translations/pt_BR.ts
    #	src/qt_gui/translations/pt_PT.ts
    #	src/qt_gui/translations/ro_RO.ts
    #	src/qt_gui/translations/ru_RU.ts
    #	src/qt_gui/translations/sl_SI.ts
    #	src/qt_gui/translations/sq_AL.ts
    #	src/qt_gui/translations/sr_CS.ts
    #	src/qt_gui/translations/sv_SE.ts
    #	src/qt_gui/translations/tr_TR.ts
    #	src/qt_gui/translations/uk_UA.ts
    #	src/qt_gui/translations/ur_PK.ts
    #	src/qt_gui/translations/vi_VN.ts
    #	src/qt_gui/translations/zh_CN.ts
    #	src/qt_gui/translations/zh_TW.ts

--- src/qt_gui/gui_settings.cpp
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: Copyright 2025 shadPS4 Emulator Project
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#include "gui_settings.h"
-
-gui_settings::gui_settings(QObject* parent) : settings(parent) {
-    m_settings = std::make_unique<QSettings>(ComputeSettingsDir() + "qt_ui.ini",
-                                             QSettings::Format::IniFormat, parent);
-}
