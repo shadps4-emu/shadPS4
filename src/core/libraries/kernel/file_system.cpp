750493a83b3efc07ff0739e269e4d8cfa6fc5c31
InvoxiPlayGames <webmaster@invoxiplaygames.uk>
2025-11-06 18:38:36 +0000

    filesystem: return st_mtim in posix_stat

--- src/core/libraries/kernel/file_system.cpp
+++ src/core/libraries/kernel/file_system.cpp
@@ -645,12 +645,20 @@ s32 PS4_SYSV_ABI posix_stat(const char* path, OrbisKernelStat* sb) {
         sb->st_size = 65536;
         sb->st_blksize = 65536;
         sb->st_blocks = 128;
+
+        const auto mtime = std::filesystem::last_write_time(path_name);
+        const auto mtimestamp = std::chrono::clock_cast<std::chrono::system_clock>(mtime);
+        sb->st_mtim.tv_sec = std::chrono::system_clock::to_time_t(mtimestamp);
         // TODO incomplete
     } else {
         sb->st_mode = 0000777u | 0100000u;
         sb->st_size = static_cast<s64>(std::filesystem::file_size(path_name));
         sb->st_blksize = 512;
         sb->st_blocks = (sb->st_size + 511) / 512;
+
+        const auto mtime = std::filesystem::last_write_time(path_name);
+        const auto mtimestamp = std::chrono::clock_cast<std::chrono::system_clock>(mtime);
+        sb->st_mtim.tv_sec = std::chrono::system_clock::to_time_t(mtimestamp);
         // TODO incomplete
     }
 
