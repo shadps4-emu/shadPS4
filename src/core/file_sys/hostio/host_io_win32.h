f603db3abbcea7bb882997ae77fc16f4c6f4a418
marecl <fotelpl@gmail.com>
2025-11-27 14:30:02 +0100

    symlinks on *dows

--- src/core/file_sys/hostio/host_io_win32.h
+++ src/core/file_sys/hostio/host_io_win32.h
@@ -5,6 +5,7 @@
 #ifdef _WIN32
 
 #include <cstdint>
+#include <unordered_map>
 #include <fcntl.h>
 #include <windows.h>
 
@@ -14,6 +15,9 @@
 namespace HostIODriver {
 
 class HostIO_Win32 final : public HostIO_Base {
+
+    std::unordered_map<fs::path, fs::path> symlinks;
+    fs::path GetSymlink(fs::path path);
 
 public:
     //
@@ -50,21 +54,13 @@ public:
         if (quasi_flags & QUASI_O_APPEND)
             flags |= _O_APPEND;
 
-        // No info, maybe there are some equivalents
-        // if (quasi_flags & QUASI_O_NONBLOCK)
-        //     flags |= _O_NONBLOCK;
-        // if (quasi_flags & QUASI_O_SYNC)
-        //     flags |= _O_SYNC;
-        // if (quasi_flags & QUASI_O_FSYNC)
-        //     flags |= _O_FSYNC;
-
         if (quasi_flags & QUASI_O_DIRECTORY)
             flags |= _O_OBTAIN_DIR; // I don't like how this one looks
 
-        // if (quasi_flags & QUASI_O_DIRECT)
-        //     flags |= _O_DIRECT;
-        // if (quasi_flags & QUASI_O_DSYNC)
-        //     flags |= _O_DSYNC;
+        if (int status = quasi_flags & (QUASI_O_NONBLOCK | QUASI_O_SYNC | QUASI_O_FSYNC |
+                                        QUASI_O_DIRECT | QUASI_O_DSYNC);
+            0 != status)
+            LOG_WARNING(Kernel_Fs, "Received unknown or unsupported flags: {:x}", status);
 
         return flags;
     }
