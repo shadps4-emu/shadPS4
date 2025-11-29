85d4fef6c95b5e83621219de583a7b98f4d37121
georgemoralis <giorgosmrls@gmail.com>
2025-11-29 20:10:12 +0200

    fixed linux?

--- src/core/emulator_settings.cpp
+++ src/core/emulator_settings.cpp
@@ -16,7 +16,7 @@ namespace nlohmann {
 template <>
 struct adl_serializer<std::filesystem::path> {
     static void to_json(json& j, const std::filesystem::path& p) {
-        j = p.u8string();
+        j = p.string();
     }
     static void from_json(const json& j, std::filesystem::path& p) {
         p = j.get<std::string>();
