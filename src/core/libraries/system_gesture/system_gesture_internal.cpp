2b317d9b8ba9e8708e8018fc0b6974808c380d2d
georgemoralis <giorgosmrls@gmail.com>
2026-01-03 12:10:38 +0200

    sceSystemGestureGetTouchRecognizerInformationInternal

--- src/core/libraries/system_gesture/system_gesture_internal.cpp
+++ src/core/libraries/system_gesture/system_gesture_internal.cpp
@@ -4,11 +4,13 @@
 
 #include <common/logging/log.h>
 #include <core/libraries/error_codes.h>
+#include <magic_enum/magic_enum.hpp>
 #include "system_gesture.h"
 #include "system_gesture_error.h"
 #include "system_gesture_internal.h"
 
 namespace Libraries::SystemGesture {
+
 s32 PS4_SYSV_ABI sceSystemGestureUpdateTouchRecognizerRectangleInternal(
     s32 handle_index, OrbisSystemGestureTouchRecognizer* touchRecognizer,
     OrbisSystemGestureRectangle* rectangle) {
@@ -55,4 +57,57 @@ s32 PS4_SYSV_ABI sceSystemGestureUpdateTouchRecognizerRectangleInternal(
 
     return ORBIS_OK;
 }
+
+s32 PS4_SYSV_ABI sceSystemGestureGetTouchRecognizerInformationInternal(
+    s32 handle_index, OrbisSystemGestureTouchRecognizer* touchRecognizer,
+    OrbisSystemGestureTouchRecognizerInformation* information) {
+
+    LOG_TRACE(Lib_SystemGesture, "Called, handle_index={}, touchRecognizer={:p}, information={:p}",
+              handle_index, fmt::ptr(touchRecognizer), fmt::ptr(information));
+
+    // Check if handle_index is valid (0-7)
+    if (handle_index >= 8) {
+        LOG_ERROR(Lib_SystemGesture, "Invalid handle_index={}, must be < 8", handle_index);
+        return ORBIS_SYSTEM_GESTURE_ERROR_INVALID_HANDLE;
+    }
+
+    if (touchRecognizer == nullptr) {
+        LOG_ERROR(Lib_SystemGesture, "touchRecognizer is null");
+        return ORBIS_SYSTEM_GESTURE_ERROR_INVALID_ARGUMENT;
+    }
+
+    if (information == nullptr) {
+        LOG_ERROR(Lib_SystemGesture, "information is null");
+        return ORBIS_SYSTEM_GESTURE_ERROR_INVALID_ARGUMENT;
+    }
+
+    // Check magic value in touchRecognizer
+    if (touchRecognizer->magic != 0x35547435) { // ASCII "5Tt5"
+        LOG_ERROR(Lib_SystemGesture,
+                  "Invalid touchRecognizer magic, expected 0x35547435, got 0x{:x}",
+                  touchRecognizer->magic);
+        return ORBIS_SYSTEM_GESTURE_ERROR_INVALID_ARGUMENT;
+    }
+    // Copy gesture type
+    information->gestureType = touchRecognizer->gestureType;
+
+    // Copy update time
+    information->updatedTime = touchRecognizer->updatedTime;
+
+    // Copy rectangle information
+    information->rectangle.x = touchRecognizer->rectangle_x;
+    information->rectangle.y = touchRecognizer->rectangle_y;
+    information->rectangle.width = touchRecognizer->rectangle_width;
+    information->rectangle.height = touchRecognizer->rectangle_height;
+
+    LOG_TRACE(Lib_SystemGesture,
+              "Retrieved touch recognizer information: gestureType=(0x{:x}), "
+              "updateTime={}, rectangle x={}, y={}, width={}, height={}",
+               magic_enum::enum_integer(information->gestureType), information->updatedTime, information->rectangle.x,
+              information->rectangle.y, information->rectangle.width,
+              information->rectangle.height);
+
+    return ORBIS_OK;
+}
+
 } // namespace Libraries::SystemGesture
--- src/core/libraries/system_gesture/system_gesture_internal.h
+++ src/core/libraries/system_gesture/system_gesture_internal.h
@@ -7,9 +7,13 @@
 
 struct OrbisSystemGestureTouchRecognizer;
 struct OrbisSystemGestureRectangle;
+struct OrbisSystemGestureTouchRecognizerInformation;
 
 namespace Libraries::SystemGesture {
 s32 sceSystemGestureUpdateTouchRecognizerRectangleInternal(
     s32 handle_index, OrbisSystemGestureTouchRecognizer* touchRecognizer,
     OrbisSystemGestureRectangle* rectangle); // FUN_010054c0
-}
+s32 PS4_SYSV_ABI sceSystemGestureGetTouchRecognizerInformationInternal(
+    s32 handle_index, OrbisSystemGestureTouchRecognizer* touchRecognizer,
+    OrbisSystemGestureTouchRecognizerInformation* information); // FUN_01005500
+} // namespace Libraries::SystemGesture
