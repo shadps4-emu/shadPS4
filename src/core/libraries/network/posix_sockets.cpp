8f5d6cd9db724e79bb0f036c6f33e098896eeeac
georgemoralis <giorgosmrls@gmail.com>
2026-01-04 22:29:09 +0200

    make kalapsofos happy

--- src/core/libraries/network/posix_sockets.cpp
+++ src/core/libraries/network/posix_sockets.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
+// SPDX-FileCopyrightText: Copyright 2024-2026 shadPS4 Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <vector>
