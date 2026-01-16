d49e72b018ec11afc113b0f111f6bdea4ebfb736
lizzie <lizzie@eden-emu.dev>
2026-01-16 06:33:11 +0000

    date-tz for fbsd, force submodule zydis

--- src/core/libraries/kernel/time.cpp
+++ src/core/libraries/kernel/time.cpp
@@ -500,10 +500,8 @@ s32 PS4_SYSV_ABI sceKernelConvertUtcToLocaltime(time_t time, time_t* local_time,
     if (dst_sec != nullptr) {
         *dst_sec = res == TIME_ZONE_ID_DAYLIGHT ? -_dstbias : 0;
     }
-#elif defined(__FreeBSD__)
-    // todo
 #else
-#if defined(__APPLE__)
+#if defined(__APPLE__) || defined(__FreeBSD__)
     // std::chrono::current_zone() not available yet.
     const auto* time_zone = date::current_zone();
 #else
