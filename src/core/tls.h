3dee2ae90d45f097068df71e0a7dc373cc47bd8b
Valdis Bogdāns <valdis.bogdans@gmail.com>
2026-02-14 21:48:23 +0200

    Fix condition to check tcb before clearing stack

--- src/core/tls.h
+++ src/core/tls.h
@@ -62,7 +62,7 @@ ReturnType ExecuteGuest(PS4_SYSV_ABI ReturnType (*func)(FuncArgs...), CallArgs&&
     EnsureThreadInitialized();
     // clear stack to avoid trash from EnsureThreadInitialized
     auto* tcb = GetTcbBase();
-    if (tcb == nullptr && tcb->tcb_fiber == nullptr) {
+    if (tcb != nullptr && tcb->tcb_fiber == nullptr) {
         ClearStack<12_KB>();
     }
     return func(std::forward<CallArgs>(args)...);
