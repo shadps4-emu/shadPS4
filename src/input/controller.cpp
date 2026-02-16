d0ea7f04c4d47ba1af181a690e75f0b608bca272
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2026-02-16 17:44:42 +0100

    fix macos

--- src/input/controller.cpp
+++ src/input/controller.cpp
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: Copyright 2024-2026 shadPS4 Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include <sstream>
 #include <unordered_set>
 #include <SDL3/SDL.h>
 #include <common/singleton.h>
