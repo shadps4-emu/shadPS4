636efaf2b550d3762037ad081197f0f3dc1490bb
georgemoralis <giorgosmrls@gmail.com>
2026-03-01 20:49:55 +0200

    changed readbacks mode to Relaxed,Precised (#4091)

--- src/video_core/buffer_cache/region_manager.h
+++ src/video_core/buffer_cache/region_manager.h
@@ -95,7 +95,7 @@ public:
         }
         if constexpr (type == Type::CPU) {
             UpdateProtection<!enable, false>();
-        } else if (Config::getReadbacksMode() == Config::GpuReadbacksMode::High) {
+        } else if (Config::getReadbacksMode() == Config::GpuReadbacksMode::Precised) {
             UpdateProtection<enable, true>();
         }
     }
