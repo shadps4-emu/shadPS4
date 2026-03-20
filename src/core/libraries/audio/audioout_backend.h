65f72c0cc4811171aeb557ded17820dc3c1b7db2
georgemoralis <giorgosmrls@gmail.com>
2026-03-20 17:45:51 +0200

    initial openal backend

--- src/core/libraries/audio/audioout_backend.h
+++ src/core/libraries/audio/audioout_backend.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
+// SPDX-FileCopyrightText: Copyright 2024-2026 shadPS4 Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
@@ -27,6 +27,11 @@ public:
 };
 
 class SDLAudioOut final : public AudioOutBackend {
+public:
+    std::unique_ptr<PortBackend> Open(PortOut& port) override;
+};
+
+class OpenALAudioOut final : public AudioOutBackend {
 public:
     std::unique_ptr<PortBackend> Open(PortOut& port) override;
 };
