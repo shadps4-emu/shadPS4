dc22a4af87e7d716e3bca39eab8eaf54f729200e
Connor Garey <connorgarey1999@gmail.com>
2026-03-27 23:04:07 +0000

    Initial devshell creation.

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "shadPS4 Nix Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    pkgsLinux = nixpkgs.legacyPackages.x86_64-linux;
+  in 
+  {
+    devShells.x86_64-linux.default = pkgsLinux.mkShell {
+      packages = with pkgsLinux; [
+        clang-tools
+        cmake
+        pkg-config
+        vulkan-tools
+        
+        renderdoc
+        gef
+        strace
+
+        sdl3.dev
+        openal
+        zlib.dev
+        libedit.dev
+        vulkan-headers
+        vulkan-utility-libraries
+        ffmpeg.dev
+        fmt.dev
+        glslang.dev
+        libxkbcommon
+        wayland.dev
+        libxcb.dev
+        stb
+        libpng.dev
+      ];
+
+      shellHook = ''
+        echo "Entering shadPS4 development shell!"
+      '';
+    };
+  };
+}
