54fa7f7cc33c7669ce955d86ef2c61c6a8a101c1
Connor Garey <connorgarey1999@gmail.com>
2026-03-28 11:28:05 +0000

    Nix format.

--- flake.nix
+++ flake.nix
@@ -5,69 +5,70 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: 
-  let
-    pkgsLinux = nixpkgs.legacyPackages.x86_64-linux;
-  in 
-  {
-    devShells.x86_64-linux.default = pkgsLinux.mkShell.override {stdenv = pkgsLinux.clangStdenv; } {
-      packages = with pkgsLinux; [
-        clang-tools
-        cmake
-        pkg-config
-        vulkan-tools
-        
-        renderdoc
-        gef
-        strace
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgsLinux = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = pkgsLinux.mkShell.override { stdenv = pkgsLinux.clangStdenv; } {
+        packages = with pkgsLinux; [
+          clang-tools
+          cmake
+          pkg-config
+          vulkan-tools
 
-        openal
-        zlib.dev
-        libedit.dev
-        vulkan-headers
-        vulkan-utility-libraries
-        ffmpeg.dev
-        fmt.dev
-        glslang.dev
-        wayland.dev
-        stb
-        libpng.dev
-        
-        # Specific SDL3 dependencies:
-        sdl3.dev
-        alsa-lib
-        hidapi
-        ibus.dev
-        jack2.dev
-        libdecor.dev
-        libthai.dev
-        fribidi.dev
-        libxcb.dev
-        libGL.dev
-        libpulseaudio.dev
-        libusb1.dev
-        libx11.dev
-        libxcursor.dev
-        libxext
-        libxfixes.dev
-        libxi.dev
-        libxinerama.dev
-        libxkbcommon
-        libxrandr.dev
-        libxrender.dev
-        libxtst
-        pipewire.dev
-        libxscrnsaver
-        sndio
-      ];
+          renderdoc
+          gef
+          strace
 
-      LD_LIBRARY_PATH = pkgsLinux.lib.makeLibraryPath [
-        pkgsLinux.mesa
-      ];
+          openal
+          zlib.dev
+          libedit.dev
+          vulkan-headers
+          vulkan-utility-libraries
+          ffmpeg.dev
+          fmt.dev
+          glslang.dev
+          wayland.dev
+          stb
+          libpng.dev
 
-      shellHook = ''
-        echo "Entering shadPS4 development shell!"
-      '';
+          # Specific SDL3 dependencies:
+          sdl3.dev
+          alsa-lib
+          hidapi
+          ibus.dev
+          jack2.dev
+          libdecor.dev
+          libthai.dev
+          fribidi.dev
+          libxcb.dev
+          libGL.dev
+          libpulseaudio.dev
+          libusb1.dev
+          libx11.dev
+          libxcursor.dev
+          libxext
+          libxfixes.dev
+          libxi.dev
+          libxinerama.dev
+          libxkbcommon
+          libxrandr.dev
+          libxrender.dev
+          libxtst
+          pipewire.dev
+          libxscrnsaver
+          sndio
+        ];
+
+        LD_LIBRARY_PATH = pkgsLinux.lib.makeLibraryPath [
+          pkgsLinux.mesa
+        ];
+
+        shellHook = ''
+          echo "Entering shadPS4 development shell!"
+        '';
+      };
     };
-  };
 }
