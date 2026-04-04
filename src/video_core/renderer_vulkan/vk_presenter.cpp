725bebc5d2c1f48dd0fe9919afcabbb3fdd0537d
Marin Baron <marin.baron.pro@gmail.com>
2026-04-04 22:09:59 +0200

    Vulkan destroy presenter members before device

--- src/video_core/renderer_vulkan/vk_presenter.cpp
+++ src/video_core/renderer_vulkan/vk_presenter.cpp
@@ -144,7 +144,6 @@ Presenter::~Presenter() {
         device.destroyImageView(frame.image_view);
         device.destroyFence(frame.present_done);
     }
-    ImGui::Core::Shutdown(device);
 }
 
 bool Presenter::IsVideoOutSurface(const AmdGpu::ColorBuffer& color_buffer) const {
