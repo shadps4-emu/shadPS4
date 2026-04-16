f98360b282f61819276037edc0b6475fe8990160
rainmakerv2 <30595646+rainmakerv3@users.noreply.github.com>
2026-04-16 15:53:39 +0800

    update comment

--- src/imgui/settings_dialog_imgui.cpp
+++ src/imgui/settings_dialog_imgui.cpp
@@ -276,7 +276,7 @@ void DrawSettings(bool* open) {
 
         SettingsCategory lastCategory =
             currentProfile != "Global" ? SettingsCategory::Experimental : SettingsCategory::Log;
-        // choose next category with R1
+        // Navigate categories with Tab / R1 / L1
         if (ImGui::IsKeyPressed(ImGuiKey_GamepadR1) || ImGui::IsKeyPressed(ImGuiKey_Tab)) {
             int currentIndex = static_cast<int>(currentCategory);
             currentCategory == lastCategory
@@ -284,7 +284,6 @@ void DrawSettings(bool* open) {
                 : currentCategory = static_cast<SettingsCategory>(currentIndex + 1);
         }
 
-        // choose previous category with R1
         if (ImGui::IsKeyPressed(ImGuiKey_GamepadL1)) {
             int currentIndex = static_cast<int>(currentCategory);
             currentIndex == 0 ? currentCategory = lastCategory
