a57b3f3b74e25191413b841b83a147014fda1e65
squidbus <175574877+squidbus@users.noreply.github.com>
2026-05-25 06:55:20 -0700

    Revert quad/rect list parameter forwarding changes (#4476)
    * Revert "shader_recompiler: Pad number of TES passthrough outputs to fragment inputs if needed"
    
    This reverts commit 64360630255eb0d2c0c23eac90efb64df989cb7b.
    
    * Revert "shader_recompiler: Only forward declared number of vertex inputs. (#4293)"
    
    This reverts commit 47f5cc14376c9d7694fc5728ed4ea98609ffe119.

--- src/shader_recompiler/backend/spirv/emit_spirv_quad_rect.cpp
+++ src/shader_recompiler/backend/spirv/emit_spirv_quad_rect.cpp
@@ -12,9 +12,8 @@ using Sirit::Id;
 constexpr u32 SPIRV_VERSION_1_5 = 0x00010500;
 
 struct QuadRectListEmitter : public Sirit::Module {
-    explicit QuadRectListEmitter(const VertexRuntimeInfo& vs_info_,
-                                 const FragmentRuntimeInfo& fs_info_)
-        : Sirit::Module{SPIRV_VERSION_1_5}, vs_info{vs_info_}, fs_info{fs_info_} {
+    explicit QuadRectListEmitter(const FragmentRuntimeInfo& fs_info_)
+        : Sirit::Module{SPIRV_VERSION_1_5}, fs_info{fs_info_} {
         void_id = TypeVoid();
         bool_id = TypeBool();
         float_id = TypeFloat(32);
@@ -25,11 +24,9 @@ struct QuadRectListEmitter : public Sirit::Module {
         vec3_id = TypeVector(float_id, 3);
         vec4_id = TypeVector(float_id, 4);
 
-        float_zero = Constant(float_id, 0.0f);
         float_one = Constant(float_id, 1.0f);
         float_min_one = Constant(float_id, -1.0f);
         int_zero = Constant(int_id, 0);
-        vec4_zero = ConstantComposite(vec4_id, float_zero, float_zero, float_zero, float_zero);
 
         const Id float_arr{TypeArray(float_id, Constant(uint_id, 1U))};
         gl_per_vertex_type = TypeStruct(vec4_id, float_id, float_arr, float_arr);
@@ -192,14 +189,8 @@ struct QuadRectListEmitter : public Sirit::Module {
         OpStore(OpAccessChain(output_vec4, gl_per_vertex, Int(0)), position);
 
         // out_paramN = in_paramN[index];
-        for (int i = 0; i < outputs.size(); i++) {
-            Id param;
-            if (i < inputs.size()) {
-                param = OpLoad(vec4_id, OpAccessChain(input_vec4, inputs[i], index));
-            } else {
-                // Not enough inputs to forward, use dummy value.
-                param = vec4_zero;
-            }
+        for (int i = 0; i < inputs.size(); i++) {
+            const Id param{OpLoad(vec4_id, OpAccessChain(input_vec4, inputs[i], index))};
             OpStore(outputs[i], param);
         }
 
@@ -245,7 +236,6 @@ private:
     }
 
     void DefineOutputs(spv::ExecutionModel model) {
-        auto num_forwards = vs_info.num_exports;
         if (model == spv::ExecutionModel::TessellationControl) {
             const Id gl_per_vertex_array{TypeArray(gl_per_vertex_type, Constant(uint_id, 4U))};
             gl_out = AddOutput(gl_per_vertex_array);
@@ -261,12 +251,9 @@ private:
             Decorate(gl_tess_level_outer, spv::Decoration::Patch);
         } else {
             gl_per_vertex = AddOutput(gl_per_vertex_type);
-            // Tessellation eval stage comes before fragment, output number of forwards it will
-            // actually accept.
-            num_forwards = fs_info.num_inputs;
         }
-        outputs.reserve(num_forwards);
-        for (int i = 0; i < num_forwards; i++) {
+        outputs.reserve(fs_info.num_inputs);
+        for (int i = 0; i < fs_info.num_inputs; i++) {
             const auto& input = fs_info.inputs[i];
             if (input.IsDefault()) {
                 continue;
@@ -289,10 +276,8 @@ private:
         const Id gl_per_vertex_array{TypeArray(gl_per_vertex_type, Constant(uint_id, 32U))};
         gl_in = AddInput(gl_per_vertex_array);
         const Id float_arr{TypeArray(vec4_id, Int(32))};
-
-        const auto num_forwards = vs_info.num_exports;
-        inputs.reserve(num_forwards);
-        for (int i = 0; i < num_forwards; i++) {
+        inputs.reserve(fs_info.num_inputs);
+        for (int i = 0; i < fs_info.num_inputs; i++) {
             const auto& input = fs_info.inputs[i];
             if (input.IsDefault()) {
                 continue;
@@ -303,7 +288,6 @@ private:
     }
 
 private:
-    VertexRuntimeInfo vs_info;
     FragmentRuntimeInfo fs_info;
     Id main;
     Id void_id;
@@ -315,11 +299,9 @@ private:
     Id vec2_id;
     Id vec3_id;
     Id vec4_id;
-    Id float_zero;
     Id float_one;
     Id float_min_one;
     Id int_zero;
-    Id vec4_zero;
     Id gl_per_vertex_type;
     Id gl_in;
     union {
@@ -337,9 +319,8 @@ private:
     std::vector<Id> interfaces;
 };
 
-std::vector<u32> EmitAuxilaryTessShader(AuxShaderType type, const VertexRuntimeInfo& vs_info,
-                                        const FragmentRuntimeInfo& fs_info) {
-    QuadRectListEmitter ctx{vs_info, fs_info};
+std::vector<u32> EmitAuxilaryTessShader(AuxShaderType type, const FragmentRuntimeInfo& fs_info) {
+    QuadRectListEmitter ctx{fs_info};
     switch (type) {
     case AuxShaderType::RectListTCS:
         ctx.EmitRectListTCS();
