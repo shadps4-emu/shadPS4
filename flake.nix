14e5e71a51ccf8b7ae4a8fe0ce0144ae975d0dec
Connor Garey <connorgarey1999@gmail.com>
2026-05-30 18:55:01 +0100

    Nix Formatting.

--- flake.nix
+++ flake.nix
@@ -16,29 +16,30 @@
     {
       formatter.x86_64-linux = pkgsLinux.nixpkgs-fmt;
 
-      devShells.x86_64-linux.default = pkgsLinux.callPackage "${self}/nix/shell.nix" {};
-      
-      packages.x86_64-linux = let
-        debugBuild = pkgsLinux.callPackage "${self}/nix/build.nix" 
-        {
-          src = "${self}";
-          system = "x86_64-linux";
-          cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Debug" ];
-        };
-        releaseBuild = pkgsLinux.callPackage "${self}/nix/build.nix" 
-        {
-          src = "${self}";
-          system = "x86_64-linux";
-          dontStrip = false;
-          cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" ];
-        };
-        releaseWithDebugInfoBuild = pkgsLinux.callPackage "${self}/nix/build.nix" 
-        {
-          src = "${self}";
-          system = "x86_64-linux";
-          cmakeFlags = [ "-DCMAKE_BUILD_TYPE=RelWithDebInfo" ];
-        };
-      in 
+      devShells.x86_64-linux.default = pkgsLinux.callPackage "${self}/nix/shell.nix" { };
+
+      packages.x86_64-linux =
+        let
+          debugBuild = pkgsLinux.callPackage "${self}/nix/build.nix"
+            {
+              src = "${self}";
+              system = "x86_64-linux";
+              cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Debug" ];
+            };
+          releaseBuild = pkgsLinux.callPackage "${self}/nix/build.nix"
+            {
+              src = "${self}";
+              system = "x86_64-linux";
+              dontStrip = false;
+              cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" ];
+            };
+          releaseWithDebugInfoBuild = pkgsLinux.callPackage "${self}/nix/build.nix"
+            {
+              src = "${self}";
+              system = "x86_64-linux";
+              cmakeFlags = [ "-DCMAKE_BUILD_TYPE=RelWithDebInfo" ];
+            };
+        in
         {
           debug = debugBuild;
           release = releaseBuild;
