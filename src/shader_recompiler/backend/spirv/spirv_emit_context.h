437e107c97c958081c966cd2e77d6cdcaa180212
Hog <hogman12333@gmail.com>
2026-06-08 20:08:39 +0100

    Add bary_coord_nopersp_sample ID and fix namespace formatting

--- src/shader_recompiler/backend/spirv/spirv_emit_context.h
+++ src/shader_recompiler/backend/spirv/spirv_emit_context.h
@@ -289,6 +289,7 @@ public:
     Id bary_coord_smooth_centroid{};
     Id bary_coord_smooth_sample{};
     Id bary_coord_nopersp{};
+    Id bary_coord_nopersp_sample{};
 
     struct TextureDefinition {
         const VectorIds* data_types;
