0fcd278f9d94180d0b9d6388645ab0ee749cb01e
georgemoralis <giorgosmrls@gmail.com>
2026-06-23 17:38:10 +0300

    fixed compile

--- src/core/emulator_settings.h
+++ src/core/emulator_settings.h
@@ -216,7 +216,7 @@ NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(GeneralSettings, install_dirs, addon_install_
                                    trophy_notification_side, connected_to_network,
                                    discord_rpc_enabled, show_fps_counter, console_language,
                                    big_picture_scale, shadnet_server, shadnet_webapi_server,
-                                   shadnet_signaling_server, enable_upnp)
+                                   signaling_info, enable_upnp)
 
 // -------------------------------
 // Log settings
