c524ba53f0f72303ff43da4b7cf9ecc6d5a10b9a
NotAnEnergyDrinkAddict <215243710+notanenergydrinkaddict@users.noreply.github.com>
2026-06-26 19:43:31 +1200

    Reapply "Memory Patch Fixes (#4619)" (#4620)
    This reverts commit 221efa40c74fa6a20e202039d9b0b59b5fa7b112.

--- src/common/memory_patcher.h
+++ src/common/memory_patcher.h
@@ -26,12 +26,12 @@ enum PatchMask : uint8_t {
 };
 
 struct patchInfo {
-    std::string gameSerial;
-    std::string modNameStr;
-    std::string offsetStr;
-    std::string valueStr;
-    std::string targetStr;
-    std::string sizeStr;
+    const std::string& gameSerial;
+    const std::string& modNameStr;
+    const std::string& offsetStr;
+    const std::string& valueStr;
+    const std::string& targetStr;
+    const std::string& sizeStr;
     bool isOffset;
     bool littleEndian;
     PatchMask patchMask;
@@ -41,11 +41,9 @@ struct patchInfo {
 std::string convertValueToHex(const std::string type, const std::string valueStr);
 
 void OnGameLoaded();
-void AddPatchToQueue(patchInfo patchToAdd);
+void AddPatchToQueue(const patchInfo& patchToAdd);
 
-void PatchMemory(std::string modNameStr, std::string offsetStr, std::string valueStr,
-                 std::string targetStr, std::string sizeStr, bool isOffset, bool littleEndian,
-                 PatchMask patchMask = PatchMask::None, int maskOffset = 0);
+void PatchMemory(const patchInfo& patch);
 
 static std::vector<int32_t> PatternToByte(const std::string& pattern);
 uintptr_t PatternScan(const std::string& signature);
