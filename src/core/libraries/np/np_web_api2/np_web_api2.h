6063cedcfffc4b5d9d9a2fdf6f34b0e7367479d2
Stephen Miller <56742918+StevenMiller123@users.noreply.github.com>
2026-07-03 19:00:51 -0500

    Define remaining parts

--- src/core/libraries/np/np_web_api2/np_web_api2.h
+++ src/core/libraries/np/np_web_api2/np_web_api2.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "common/types.h"
+#include "core/libraries/np/np_types.h"
 
 namespace Core::Loader {
 class SymbolsResolver;
@@ -29,17 +30,17 @@ struct OrbisNpWebApi2IntInitialize2Args {
     u64 struct_size;
 };
 
+struct OrbisNpWebApi2ContentParameter {
+    u64 content_length;
+    const char* content_type;
+    u8 reserved[16];
+};
+
 struct OrbisNpWebApi2MemoryPoolStats {
     u64 pool_size;
     u64 max_inuse_size;
     u64 current_inuse_size;
     s32 reserved;
-};
-
-struct OrbisNpWebApi2ContentParameter {
-    u64 content_length;
-    const char* content_type;
-    u8 reserved[16];
 };
 
 struct OrbisNpWebApi2ResponseInformationOption {
@@ -63,6 +64,39 @@ struct OrbisNpWebApi2PushEventFilterParameter {
     OrbisNpWebApi2PushEventExtdDataKey* extd_data_key;
     u64 extd_data_key_num;
 };
+
+struct OrbisNpWebApi2PushEventExtdData {
+    OrbisNpWebApi2PushEventExtdDataKey extd_data_key;
+    char* data;
+    u64 data_len;
+};
+
+using OrbisNpWebApi2PushEventCallback = void PS4_SYSV_ABI (*)(
+    s32 user_ctx_id, s32 callback_id, const char* np_service_name,
+    OrbisNpServiceLabel np_service_label, const OrbisNpPeerAddressA* to,
+    const OrbisNpOnlineId* to_online_id, const OrbisNpPeerAddressA* from,
+    const OrbisNpOnlineId* from_online_id, const OrbisNpWebApi2PushEventDataType* data_type,
+    const char* data, u64 data_len, const OrbisNpWebApi2PushEventExtdData* extd_data,
+    u64 extd_data_num, void* user_arg);
+
+enum OrbisNpWebApi2PushEventPushContextCallbackType {
+    Unknown = -1,
+    Received = 0,
+    Dropped = 1,
+};
+
+struct OrbisNpWebApi2PushEventPushContextId {
+    char uuid[37];
+};
+
+using OrbisNpWebApi2PushEventPushContextCallback = void PS4_SYSV_ABI (*)(
+    s32 user_ctx_id, s32 callback_id, const OrbisNpWebApi2PushEventPushContextId* push_ctx_id,
+    OrbisNpWebApi2PushEventPushContextCallbackType cb_type, const char* np_service_name,
+    OrbisNpServiceLabel np_service_label, const OrbisNpPeerAddressA* to,
+    const OrbisNpOnlineId* to_online_id, const OrbisNpPeerAddressA* from,
+    const OrbisNpOnlineId* from_online_id, const OrbisNpWebApi2PushEventDataType* data_type,
+    const char* data, u64 data_len, const OrbisNpWebApi2PushEventExtdData* extd_data,
+    u64 extd_data_num, void* user_arg);
 
 void RegisterLib(Core::Loader::SymbolsResolver* sym);
 } // namespace Libraries::Np::NpWebApi2
