0afb317afeb0589a7cea9786f95142b3c46cae26
Kravickas <joker46@seznam.cz>
2026-07-25 23:49:48 +0200

    video_core: Implement T# min_lod (VK_EXT_image_view_min_lod) (#4756)
    * Implement min_lod support in image view creation
    
    * Add min_lod member to ImageViewInfo struct
    
    * Add ImageViewMinLod feature support
    
    * Add support check for minLod feature
    
    * log
    
    * Update FindView to use modified lookup_info
    
    * cleanup
    
    * Adress comments
    
    more cleanup

--- src/video_core/renderer_vulkan/vk_instance.h
+++ src/video_core/renderer_vulkan/vk_instance.h
@@ -429,6 +429,11 @@ public:
                image_2d_view_of_3d_features.sampler2DViewOf3D;
     }
 
+    /// Returns whether VK_EXT_image_view_min_lod is supported.
+    bool IsImageViewMinLodSupported() const {
+        return image_view_min_lod;
+    }
+
     /// Returns whether the device can report memory usage.
     bool CanReportMemoryUsage() const {
         return supports_memory_budget;
@@ -513,6 +518,7 @@ private:
     bool maintenance_8{};
     bool attachment_feedback_loop{};
     bool image_2d_view_of_3d{};
+    bool image_view_min_lod{};
     bool supports_memory_budget{};
     bool supports_block_texel_view{};
     u64 total_memory_budget{};
