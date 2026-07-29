f8cf383e95ecef471b6c61e918f1d11a5b20a0f0
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2026-07-29 20:24:51 +0200

    remove obsolete error message

--- src/core/libraries/kernel/threads/event_flag.cpp
+++ src/core/libraries/kernel/threads/event_flag.cpp
@@ -288,10 +288,6 @@ int PS4_SYSV_ABI sceKernelCreateEventFlag(OrbisKernelEventFlag* ef, const char* 
         UNREACHABLE();
     }
 
-    if (queue_mode == EventFlagInternal::QueueMode::ThreadPrio) {
-        LOG_ERROR(Kernel_Event, "ThreadPriority attr is not supported!");
-    }
-
     *ef = new EventFlagInternal(std::string(pName), thread_mode, queue_mode, initPattern);
     return ORBIS_OK;
 }
