95c369986533fff4b29ede4c11f77c1adda6035f
Kody Schram <kody.schram@icloud.com>
2026-08-04 14:30:25 -0700

    Added explicit include of ctime, cleaned variable initialization

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -1,11 +1,10 @@
 // SPDX-FileCopyrightText: Copyright 2025-2026 shadPS4 Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include <chrono>
-#include <ctime>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <ctime>
 #include <set>
 #include <sstream>
 #include <fmt/core.h>
@@ -852,14 +851,13 @@ void Emulator::UpdatePlayTime(const std::string& serial) {
 
     std::string playTimeSaved = fmt::format("{:d}:{:02d}:{:02d}", hours, minutes, seconds);
 
-	std::time_t last_time_played; 
-	std::time(&last_time_played);
+	const std::time_t last_time_played = std::time(nullptr);
 
     std::ofstream outfile(filePath, std::ios::trunc);
     bool lineUpdated = false;
     for (const auto& l : lines) {
         std::istringstream iss(l);
-			std::string s;
+		std::string s;
         if (iss >> s && s == serial) {
             outfile << fmt::format("{} {} {}\n", serial, playTimeSaved, last_time_played);
             lineUpdated = true;
