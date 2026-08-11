15e71316a73bda7ddfb8726850911a53e58943f7
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2026-08-11 14:14:42 +0200

    Export sceKernelLoadStartModuleInternalForMono

--- src/core/libraries/kernel/process.cpp
+++ src/core/libraries/kernel/process.cpp
@@ -295,6 +295,8 @@ void RegisterProcess(Core::Loader::SymbolsResolver* sym) {
     LIB_FUNCTION("g0VTBxfJyu0", "libkernel", 1, "libkernel", sceKernelGetCurrentCpu);
     LIB_FUNCTION("959qrazPIrg", "libkernel", 1, "libkernel", sceKernelGetProcParam);
     LIB_FUNCTION("wzvqT4UqKX8", "libkernel", 1, "libkernel", sceKernelLoadStartModule);
+    LIB_FUNCTION("Y1nEpkCieOY", "libkernel", 1, "libkernel",
+                 sceKernelLoadStartModule); // sceKernelLoadStartModuleInternalForMono
     LIB_FUNCTION("LwG8g3niqwA", "libkernel", 1, "libkernel", sceKernelDlsym);
     LIB_FUNCTION("RpQJJVKTiFM", "libkernel", 1, "libkernel", sceKernelGetModuleInfoForUnwind);
     LIB_FUNCTION("f7KBOafysXo", "libkernel", 1, "libkernel", sceKernelGetModuleInfoFromAddr);
