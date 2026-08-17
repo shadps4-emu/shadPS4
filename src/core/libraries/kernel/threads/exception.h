f3c4289317ce243bf077d69fe9f48c3f4e10ea69
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2026-08-17 09:37:22 +0200

    factor out signal related defines into a header and add si_code-s

--- src/core/libraries/kernel/threads/exception.h
+++ src/core/libraries/kernel/threads/exception.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "common/types.h"
+#include "exception_codes.h"
 
 #ifndef _WIN32
 #include <sys/signal.h>
@@ -21,54 +22,6 @@ struct OrbisKernelExceptionHandlerStack {
     u64 ss_size;
     u32 ss_flags;
 };
-
-constexpr s32 POSIX_SS_ONSTACK = 0x0001; /* take signal on alternate stack */
-constexpr s32 POSIX_SS_DISABLE = 0x0004; /* disable taking signals on alternate stack */
-
-constexpr s32 POSIX_SIGHUP = 1;
-constexpr s32 POSIX_SIGINT = 2;
-constexpr s32 POSIX_SIGQUIT = 3;
-constexpr s32 POSIX_SIGILL = 4;
-constexpr s32 POSIX_SIGTRAP = 5;
-constexpr s32 POSIX_SIGABRT = 6;
-constexpr s32 POSIX_SIGEMT = 7;
-constexpr s32 POSIX_SIGFPE = 8;
-constexpr s32 POSIX_SIGKILL = 9;
-constexpr s32 POSIX_SIGBUS = 10;
-constexpr s32 POSIX_SIGSEGV = 11;
-constexpr s32 POSIX_SIGSYS = 12;
-constexpr s32 POSIX_SIGPIPE = 13;
-constexpr s32 POSIX_SIGALRM = 14;
-constexpr s32 POSIX_SIGTERM = 15;
-constexpr s32 POSIX_SIGURG = 16;
-constexpr s32 POSIX_SIGSTOP = 17;
-constexpr s32 POSIX_SIGTSTP = 18;
-constexpr s32 POSIX_SIGCONT = 19;
-constexpr s32 POSIX_SIGCHLD = 20;
-constexpr s32 POSIX_SIGTTIN = 21;
-constexpr s32 POSIX_SIGTTOU = 22;
-constexpr s32 POSIX_SIGIO = 23;
-constexpr s32 POSIX_SIGXCPU = 24;
-constexpr s32 POSIX_SIGXFSZ = 25;
-constexpr s32 POSIX_SIGVTALRM = 26;
-constexpr s32 POSIX_SIGPROF = 27;
-constexpr s32 POSIX_SIGWINCH = 28;
-constexpr s32 POSIX_SIGINFO = 29;
-constexpr s32 POSIX_SIGUSR1 = 30;
-constexpr s32 POSIX_SIGUSR2 = 31;
-constexpr s32 POSIX_SIGTHR = 32;
-constexpr s32 POSIX_SIGLIBRT = 33;
-
-constexpr s32 POSIX_SA_ONSTACK = 0x0001;   /* take signal on signal stack */
-constexpr s32 POSIX_SA_RESTART = 0x0002;   /* restart system call on signal return */
-constexpr s32 POSIX_SA_RESETHAND = 0x0004; /* reset to SIG_DFL when taking signal */
-constexpr s32 POSIX_SA_NODEFER = 0x0010;   /* don't mask the signal we're delivering */
-constexpr s32 POSIX_SA_NOCLDWAIT = 0x0020; /* don't keep zombies around */
-constexpr s32 POSIX_SA_SIGINFO = 0x0040;   /* signal handler with SA_SIGINFO args */
-
-constexpr s32 POSIX_SIG_BLOCK = 1;   /* block specified signal set */
-constexpr s32 POSIX_SIG_UNBLOCK = 2; /* unblock specified signal set */
-constexpr s32 POSIX_SIG_SETMASK = 3; /* set specified signal set */
 
 struct Mcontext {
     u64 mc_onstack;
@@ -169,9 +122,6 @@ struct Siginfo {
     } _reason;
 };
 
-constexpr s32 POSIX_SI_NOINFO = 0;
-constexpr s32 POSIX_SI_LWP = 0x10007;
-
 struct Ucontext {
     struct Sigset uc_sigmask;
     int field1_0x10[12];
@@ -202,9 +152,6 @@ struct Sigaction {
     int sa_flags;
     Sigset sa_mask;
 };
-
-constexpr uintptr_t POSIX_SIG_DFL = 0;
-constexpr uintptr_t POSIX_SIG_IGN = 1;
 
 s32 NativeToOrbisSignal(s32 s);
 
