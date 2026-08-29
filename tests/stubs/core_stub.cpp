909a28bc67ae7692100559703519fcbaf7dc3a94
Valdis Bogdāns <valdis.bogdans@gmail.com>
2026-08-30 01:04:09 +0300

    fix tests

--- tests/stubs/core_stub.cpp
+++ tests/stubs/core_stub.cpp
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: Copyright 2026 shadPS4 Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include "core/signals.h"
 #include "emulator.h"
 
 namespace Core {
@@ -9,9 +8,5 @@ namespace Core {
 Emulator::Emulator() {}
 Emulator::~Emulator() {}
 void Emulator::Shutdown() {}
-
-SignalDispatch::SignalDispatch() {}
-SignalDispatch::~SignalDispatch() {}
-void SignalDispatch::RemoveHandlers() {}
 
 } // namespace Core
