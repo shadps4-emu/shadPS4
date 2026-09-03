2eca0b5f0b7555c7ca67e043fbbc03ac4b2f7f9d
georgemoralis <giorgosmrls@gmail.com>
2026-09-03 15:40:23 +0300

    Upstreaming imgui  (#4960)
    * added imgui upstream and updated ImGuiFileDialog with a needed update
    
    * cmake fix
    
    * update ImGuiChildFlags ,ImGuiWindowFlags in latest imgui
    
    * revert to recheck
    
    * big picture flags
    
    * fixed texture descriptor
    
    * migrated layer_extra to latest imgui
    
    * updated text_editor to latest imgui

--- src/imgui/big_picture/settings_dialog_imgui.cpp
+++ src/imgui/big_picture/settings_dialog_imgui.cpp
@@ -319,11 +319,13 @@ void SettingsWindow::DrawCategoryTabs() {
 
     float vertSize = (settingsIconSize * uiScale + ImGui::CalcTextSize("Profiles").y) +
                      ImGui::GetStyle().FramePadding.y * 4.f + 20.0 * uiScale;
-    ImGuiWindowFlags child_flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize |
-                                   ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NavFlattened;
-    ImGui::BeginChild("Categories", ImVec2(0, vertSize), true,
-                      child_flags | ImGuiWindowFlags_HorizontalScrollbar |
-                          ImGuiWindowFlags_NoScrollWithMouse);
+    ImGuiChildFlags child_flags = ImGuiChildFlags_Borders | ImGuiChildFlags_NavFlattened;
+
+    ImGuiWindowFlags window_flags =
+        ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse |
+        ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_NoScrollWithMouse;
+
+    ImGui::BeginChild("Categories", ImVec2(0, vertSize), child_flags, window_flags);
 
     ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(30.0f * uiScale, 0.0f));
 
@@ -386,8 +388,6 @@ void SettingsWindow::AddCategory(std::string name,
 void SettingsWindow::DrawMainContent(bool* open, const std::function<void()>& applySettings) {
     ImVec4 settingsColor = ImVec4(0.1f, 0.1f, 0.12f, 0.8f); // Darker gray
     ImGui::PushStyleColor(ImGuiCol_ChildBg, settingsColor);
-    ImGuiWindowFlags child_flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize |
-                                   ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NavFlattened;
 
     std::string centeredText;
     currentCategory == SettingsCategory::Folders
@@ -478,16 +478,20 @@ void SettingsWindow::DrawMainContent(bool* open, const std::function<void()>& ap
 }
 
 void SettingsWindow::DrawProfileSelector() {
-    ImGuiWindowFlags child_flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize |
-                                   ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NavFlattened;
+    ImGuiChildFlags child_flags = ImGuiChildFlags_Borders | ImGuiChildFlags_NavFlattened;
+
+    ImGuiWindowFlags window_flags =
+        ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse;
+
     ImVec4 settingsColor = ImVec4(0.1f, 0.1f, 0.12f, 0.8f); // Darker gray
     ImGui::PushStyleColor(ImGuiCol_ChildBg, settingsColor);
     ImGui::PushStyleVar(ImGuiStyleVar_ButtonTextAlign, ImVec2(0.0f, 0.5f));
     ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
     ImGui::PushStyleVar(ImGuiStyleVar_ChildBorderSize, 0.0f);
 
-    ImGui::BeginChild("Profile Selection", ImVec2(0, -ImGui::GetFrameHeightWithSpacing()), true,
-                      child_flags);
+    ImGui::BeginChild("Profile Selection", ImVec2(0, -ImGui::GetFrameHeightWithSpacing()),
+                      child_flags, window_flags);
+
     ImGui::PopStyleColor();
 
     if (ImGui::BeginTable("ProfilesTable", 2, ImGuiTableFlags_RowBg | ImGuiTableFlags_Borders)) {
@@ -582,10 +586,13 @@ void SettingsWindow::DrawProfileSelector() {
 }
 
 void SettingsWindow::DrawGameFolderManager() {
-    ImGuiWindowFlags child_flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize |
-                                   ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NavFlattened;
-    ImGui::BeginChild("ContentRegion", ImVec2(0, -ImGui::GetFrameHeightWithSpacing()), true,
-                      child_flags);
+    ImGuiChildFlags child_flags = ImGuiChildFlags_Borders | ImGuiChildFlags_NavFlattened;
+
+    ImGuiWindowFlags window_flags =
+        ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse;
+
+    ImGui::BeginChild("ContentRegion", ImVec2(0, -ImGui::GetFrameHeightWithSpacing()), child_flags,
+                      window_flags);
 
     if (ImGui::Button("Add Folder", ImVec2(400.f * uiScale, 0))) {
         ImGuiFileDialog::Instance()->OpenDialog("OpenFolder", "Add shadPS4 game folder", nullptr,
@@ -665,11 +672,15 @@ void SettingsWindow::DrawGameFolderManager() {
 
 void SettingsWindow::DrawSettingsTable(SettingsCategory category) {
     ImGui::PushStyleVar(ImGuiStyleVar_CellPadding, ImVec2(4.0f * uiScale, 10.0f * uiScale));
-    ImGuiWindowFlags child_flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize |
-                                   ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NavFlattened;
+    ImGuiChildFlags child_flags = ImGuiChildFlags_Borders | ImGuiChildFlags_NavFlattened;
 
-    ImGui::BeginChild("ContentRegion", ImVec2(0, -ImGui::GetFrameHeightWithSpacing()), true,
-                      child_flags);
+    ImGuiWindowFlags window_flags =
+        ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse;
+
+    ImGui::PushStyleVar(ImGuiStyleVar_CellPadding, ImVec2(4.0f * uiScale, 10.0f * uiScale));
+
+    ImGui::BeginChild("ContentRegion", ImVec2(0, -ImGui::GetFrameHeightWithSpacing()), child_flags,
+                      window_flags);
 
     if (category == SettingsCategory::General) {
         if (ImGui::BeginTable("SettingsTable", 2)) {
