e13675bb1f61bd9855a5001e5019bf8c8bd4d69d
Hog <hogman12333@gmail.com>
2026-09-12 20:17:47 +0100

    Add shared atomic compare and swap functions for U32 and U64

--- src/shader_recompiler/backend/spirv/emit_spirv_atomic.cpp
+++ src/shader_recompiler/backend/spirv/emit_spirv_atomic.cpp
@@ -52,6 +52,24 @@ Id SharedAtomicU64IncDec(EmitContext& ctx, Id offset,
     return (ctx.*atomic_func)(ctx.U64, pointer, scope, semantics);
 }
 
+Id SharedAtomicU32CmpSwap(EmitContext& ctx, Id offset, Id value, Id cmp_value) {
+    const Id shift_id{ctx.ConstU32(2U)};
+    const Id index{ctx.OpShiftRightLogical(ctx.U32[1], offset, shift_id)};
+    const Id pointer{ctx.EmitSharedMemoryAccess(ctx.shared_u32, ctx.shared_memory_u32, index)};
+    const auto [scope, semantics]{AtomicArgs(ctx)};
+    return ctx.OpAtomicCompareExchange(ctx.U32[1], pointer, scope, semantics, semantics, value,
+                                       cmp_value);
+}
+
+Id SharedAtomicU64CmpSwap(EmitContext& ctx, Id offset, Id value, Id cmp_value) {
+    const Id shift_id{ctx.ConstU32(3U)};
+    const Id index{ctx.OpShiftRightLogical(ctx.U32[1], offset, shift_id)};
+    const Id pointer{ctx.EmitSharedMemoryAccess(ctx.shared_u64, ctx.shared_memory_u64, index)};
+    const auto [scope, semantics]{AtomicArgs(ctx)};
+    return ctx.OpAtomicCompareExchange(ctx.U64, pointer, scope, semantics, semantics, value,
+                                       cmp_value);
+}
+
 template <bool is_float = false>
 Id BufferAtomicU32(EmitContext& ctx, IR::Inst* inst, u32 handle, Id address, Id value,
                    Id (Sirit::Module::*atomic_func)(Id, Id, Id, Id, Id)) {
@@ -199,6 +217,14 @@ Id EmitSharedAtomicISub32(EmitContext& ctx, Id offset, Id value) {
 
 Id EmitSharedAtomicISub64(EmitContext& ctx, Id offset, Id value) {
     return SharedAtomicU64(ctx, offset, value, &Sirit::Module::OpAtomicISub);
+}
+
+Id EmitSharedAtomicCmpSwap32(EmitContext& ctx, Id offset, Id value, Id cmp_value) {
+    return SharedAtomicU32CmpSwap(ctx, offset, value, cmp_value);
+}
+
+Id EmitSharedAtomicCmpSwap64(EmitContext& ctx, Id offset, Id value, Id cmp_value) {
+    return SharedAtomicU64CmpSwap(ctx, offset, value, cmp_value);
 }
 
 Id EmitSharedAtomicInc32(EmitContext& ctx, Id offset) {
