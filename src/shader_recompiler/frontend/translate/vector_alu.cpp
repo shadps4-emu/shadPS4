b1294098b7567c6b3bb02ffd4fa5251b5d8205ef
Stephen Miller <56742918+StevenMiller123@users.noreply.github.com>
2026-09-12 11:41:57 -0500

    Implement some V_CMPX_U64 ops
    Just the two I know are needed right now, can add more if others have been seen in games.

--- src/shader_recompiler/frontend/translate/vector_alu.cpp
+++ src/shader_recompiler/frontend/translate/vector_alu.cpp
@@ -381,6 +381,12 @@ void Translator::EmitVectorAlu(const GcnInst& inst) {
     case Opcode::V_CMP_LT_U64:
         return V_CMP_U64(ConditionOp::LT, false, false, inst);
 
+        //     V_CMPX_{OP8}_U64
+    case Opcode::V_CMPX_EQ_U64:
+        return V_CMP_U64(ConditionOp::EQ, false, false, inst);
+    case Opcode::V_CMPX_LG_U64:
+        return V_CMP_U64(ConditionOp::LG, false, false, inst);
+
     case Opcode::V_CMP_CLASS_F32:
         return V_CMP_CLASS_F32(inst);
 
@@ -1283,7 +1289,11 @@ void Translator::V_CMP_U64(ConditionOp op, bool is_signed, bool set_exec, const 
         UNREACHABLE_MSG("V_CMP_U64 with signed integers is not supported");
     }
     if (set_exec) {
-        UNREACHABLE_MSG("Exec setting for V_CMP_U64 is not supported");
+        // See the V_CMPX note in V_CMP_F32.
+        const IR::U1 masked{ir.LogicalAnd(ir.GetExec(), result)};
+        ir.SetExec(masked);
+        SetDst64(inst.dst[1], ir.Ballot(masked));
+        return;
     }
     SetDst64(inst.dst[1], ir.Ballot(result));
 }
