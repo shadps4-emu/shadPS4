ca6f7e56b4728b6676ed7622d26477c5353fa063
Gih-pt <296242299+Gih-pt@users.noreply.github.com>
2026-09-13 11:05:37 +0000

    shader_recompiler: Implement V_ASHR_I64 (#5011)

--- src/shader_recompiler/frontend/translate/translate.h
+++ src/shader_recompiler/frontend/translate/translate.h
@@ -275,6 +275,7 @@ public:
     void V_CVT_PK_U8_F32(const GcnInst& inst);
     void V_LSHL_B64(const GcnInst& inst);
     void V_LSHR_B64(const GcnInst& inst);
+    void V_ASHR_I64(const GcnInst& inst);
     void V_ALIGNBIT_B32(const GcnInst& inst);
     void V_ALIGNBYTE_B32(const GcnInst& inst);
     void V_MUL_F64(const GcnInst& inst);
