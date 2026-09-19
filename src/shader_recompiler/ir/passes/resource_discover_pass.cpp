712099c8a931ea4208a6b4ccd09c3dc0b1a206cb
Gih-pt <296242299+Gih-pt@users.noreply.github.com>
2026-09-19 15:14:38 -0100

    I need to get Clang to help me first.

--- src/shader_recompiler/ir/passes/resource_discover_pass.cpp
+++ src/shader_recompiler/ir/passes/resource_discover_pass.cpp
@@ -254,14 +254,14 @@ void MarkReadConstBufferSharpSources(const SharpReference& sharp) {
         if (!source) {
             continue;
         }
-        
+
         if (!IsSharpSource(source)) {
             LOG_WARNING(Render_Recompiler,
                         "Unrecognized sharp source instruction {} for dword {}, skipping",
                         magic_enum::enum_name(source->GetOpcode()), i);
             continue;
         }
-        
+
         if (source->GetOpcode() == IR::Opcode::ReadConstBuffer) {
             auto flags = source->Flags<IR::BufferInstInfo>();
             flags.sharp_source.Assign(1u);
