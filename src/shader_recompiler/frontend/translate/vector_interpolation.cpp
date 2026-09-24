2d9ddb7816aba99c528a52c2cdee9f7fc3b1ad87
Valdis Bogdāns <valdis.bogdans@gmail.com>
2026-09-25 01:52:05 +0300

    shader_recompiler: Support P10/P20 V_INTERP_MOV_F32 selectors

--- src/shader_recompiler/frontend/translate/vector_interpolation.cpp
+++ src/shader_recompiler/frontend/translate/vector_interpolation.cpp
@@ -100,13 +100,23 @@ void Translator::V_INTERP_MOV_F32(const GcnInst& inst) {
     const IR::Attribute attrib = IR::Attribute::Param0 + attr_index;
     const auto& attr = runtime_info.hw.fs.inputs[attr_index];
     auto& interp = info.fs_interpolation[attr_index];
-    ASSERT(attr.is_flat || inst.src[0].code == 2);
+    const u32 src_select = inst.src[0].code;
+    ASSERT_MSG(src_select < 3, "Invalid V_INTERP_MOV_F32 selector {}", src_select);
+    ASSERT(attr.is_flat || src_select == 2 ||
+           profile.supports_amd_shader_explicit_vertex_parameter ||
+           profile.supports_fragment_shader_barycentric);
     if (profile.supports_amd_shader_explicit_vertex_parameter ||
         profile.supports_fragment_shader_barycentric) {
         // VSRC 0=P10, 1=P20, 2=P0
         interp.primary = Qualifier::PerVertex;
-        SetDst(inst.dst[0],
-               ir.GetAttribute(attrib, inst.control.vintrp.chan, (inst.src[0].code + 1) % 3));
+        IR::F32 result = ir.GetAttribute(attrib, inst.control.vintrp.chan, (src_select + 1) % 3);
+        // is_default stores OFFSET5, which selects passthrough mode together with flat_shade.
+        const bool is_passthrough = attr.is_flat && attr.is_default;
+        if (src_select != 2 && !is_passthrough) {
+            const IR::F32 p0 = ir.GetAttribute(attrib, inst.control.vintrp.chan, 0);
+            result = ir.FPSub(result, p0);
+        }
+        SetDst(inst.dst[0], result);
     } else {
         interp.primary = Qualifier::Flat;
         SetDst(inst.dst[0], ir.GetAttribute(attrib, inst.control.vintrp.chan));
