37e851aafd79ea02f7fa1f0d7c8b2a8529cb0f72
raphaelthegreat <47210458+raphaelthegreat@users.noreply.github.com>
2026-09-25 19:17:57 +0300

    fix build pt1

--- src/core/file_sys/devices/rng_device.cpp
+++ src/core/file_sys/devices/rng_device.cpp
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: Copyright 2025 shadPS4 Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include <cstdlib>
 #include <ctime>
 #include "common/logging/log.h"
 #include "common/va_ctx.h"
