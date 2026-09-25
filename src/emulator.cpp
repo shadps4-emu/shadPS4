1254885d50d5c4f06f188405287bb840df17211b
squidbus <175574877+squidbus@users.noreply.github.com>
2026-09-25 02:24:38 -0700

    core: Support loading DLC as ZAR

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -386,49 +386,45 @@ void Emulator::Run(std::filesystem::path file, std::vector<std::string> args,
     Common::PSFAttributes psf_attributes{};
 
     if (auto psf_handle = mnt->Open("/app0/sce_sys/param.sfo", /*writable=*/false)) {
-        std::vector<u8> psf_buf(psf_handle->Size());
-        if (psf_handle->Read(psf_buf.data(), psf_buf.size()) == static_cast<s64>(psf_buf.size())) {
-            auto* param_sfo = Common::Singleton<PSF>::Instance();
-            ASSERT_MSG(param_sfo->Open(psf_buf), "Failed to open param.sfo");
-            param_sfo_exists = true;
+        auto* param_sfo = Common::Singleton<PSF>::Instance();
+        ASSERT_MSG(param_sfo->Open(psf_handle), "Failed to open param.sfo");
+        param_sfo_exists = true;
 
-            const auto content_id = param_sfo->GetString("CONTENT_ID");
-            const auto title_id = param_sfo->GetString("TITLE_ID");
-            if (content_id.has_value() && !content_id->empty()) {
-                id = std::string(*content_id, 7, 9);
-            } else if (title_id.has_value()) {
-                id = *title_id;
-            }
-            title = param_sfo->GetString("TITLE").value_or("Unknown title");
-            fw_version = param_sfo->GetInteger("SYSTEM_VER").value_or(0x4700000);
-            app_version = param_sfo->GetString("APP_VER").value_or("Unknown version");
-            if (const auto raw_attributes = param_sfo->GetInteger("ATTRIBUTE")) {
-                psf_attributes.raw = *raw_attributes;
-            }
+        const auto content_id = param_sfo->GetString("CONTENT_ID");
+        const auto title_id = param_sfo->GetString("TITLE_ID");
+        if (content_id.has_value() && !content_id->empty()) {
+            id = std::string(*content_id, 7, 9);
+        } else if (title_id.has_value()) {
+            id = *title_id;
+        }
+        title = param_sfo->GetString("TITLE").value_or("Unknown title");
+        fw_version = param_sfo->GetInteger("SYSTEM_VER").value_or(0x4700000);
+        app_version = param_sfo->GetString("APP_VER").value_or("Unknown version");
+        if (const auto raw_attributes = param_sfo->GetInteger("ATTRIBUTE")) {
+            psf_attributes.raw = *raw_attributes;
+        }
 
-            // Extract sdk version from pubtool info.
-            std::string_view pubtool_info =
-                param_sfo->GetString("PUBTOOLINFO").value_or("Unknown value");
-            u64 sdk_ver_offset = pubtool_info.find("sdk_ver");
+        // Extract sdk version from pubtool info.
+        std::string_view pubtool_info =
+            param_sfo->GetString("PUBTOOLINFO").value_or("Unknown value");
+        u64 sdk_ver_offset = pubtool_info.find("sdk_ver");
 
-            if (sdk_ver_offset == pubtool_info.npos) {
-                // Default to using firmware version if SDK version is not found.
-                sdk_version = fw_version;
-            } else {
-                // Increment offset to account for sdk_ver= part of string.
-                sdk_ver_offset += 8;
-                u64 sdk_ver_len = pubtool_info.find(",", sdk_ver_offset);
-                if (sdk_ver_len == pubtool_info.npos) {
-                    // If there's no more commas, this is likely the last entry of pubtool info.
-                    // Use string length instead.
-                    sdk_ver_len = pubtool_info.size();
-                }
-                sdk_ver_len -= sdk_ver_offset;
-                std::string sdk_ver_string =
-                    pubtool_info.substr(sdk_ver_offset, sdk_ver_len).data();
-                // Number is stored in base 16.
-                sdk_version = std::stoi(sdk_ver_string, nullptr, 16);
+        if (sdk_ver_offset == pubtool_info.npos) {
+            // Default to using firmware version if SDK version is not found.
+            sdk_version = fw_version;
+        } else {
+            // Increment offset to account for sdk_ver= part of string.
+            sdk_ver_offset += 8;
+            u64 sdk_ver_len = pubtool_info.find(",", sdk_ver_offset);
+            if (sdk_ver_len == pubtool_info.npos) {
+                // If there's no more commas, this is likely the last entry of pubtool info.
+                // Use string length instead.
+                sdk_ver_len = pubtool_info.size();
             }
+            sdk_ver_len -= sdk_ver_offset;
+            std::string sdk_ver_string = pubtool_info.substr(sdk_ver_offset, sdk_ver_len).data();
+            // Number is stored in base 16.
+            sdk_version = std::stoi(sdk_ver_string, nullptr, 16);
         }
     }
 
